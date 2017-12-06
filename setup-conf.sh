6c3235bf0e1d88d9f0c4a710d094e4b78b28afca
Cristian Stroparo <cstropz@gmail.com>
2017-12-06 12:08:10 -0600

    Fix

--- setup-conf.sh
+++ setup-conf.sh
@@ -7,4 +7,5 @@ cp -v ./conf/exrc ~/.exrc
 cp -v ./conf/vimrc ~/.vimrc
 cp -v ./conf/sshconfig ~/.ssh/config
 
+mkdir -p "$SUBLIME_PATH"/Data/Packages/User/
 cp -v ./conf/sublime3/* "$SUBLIME_PATH"/Data/Packages/User/
