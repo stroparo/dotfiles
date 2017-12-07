ef3ef9f4954759cdc3181f671b088a16d8573e32
Cristian Stroparo <Cristian Stroparo>
2017-12-07 09:55:39 -0200

    Fix

--- setup-conf.sh
+++ setup-conf.sh
@@ -7,7 +7,7 @@ cp -v ./conf/sshconfig ~/.ssh/config
 # Sublime Text:
 SUBL_WIN='C:\Users\cr391577\AppData\Roaming\Sublime Text 3'
 if [[ "$(uname -a)" = *[Cc]ygwin* ]] \
-  && [ -d `cygpath "${SUBL_WIN}" ]
+  && [ -d `cygpath "${SUBL_WIN}"` ]
 then
   SUBL_USER="${SUBL_WIN}/Packages/User"
 else
