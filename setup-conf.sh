3b063765e7ba777f75cd05692d87acf7fb18d1a8
Cristian Stroparo <Cristian Stroparo>
2017-12-07 09:54:14 -0200

    Update sublime in correct Windows path if it was installed

--- setup-conf.sh
+++ setup-conf.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env sh
 
-echo "Sublime path:"
-read SUBLIME_PATH
-
 cp -v ./conf/exrc ~/.exrc
 cp -v ./conf/vimrc ~/.vimrc
 cp -v ./conf/sshconfig ~/.ssh/config
 
-mkdir -p "$SUBLIME_PATH"/Data/Packages/User/
-cp -v ./conf/sublime3/* "$SUBLIME_PATH"/Data/Packages/User/
+# Sublime Text:
+SUBL_WIN='C:\Users\cr391577\AppData\Roaming\Sublime Text 3'
+if [[ "$(uname -a)" = *[Cc]ygwin* ]] \
+  && [ -d `cygpath "${SUBL_WIN}" ]
+then
+  SUBL_USER="${SUBL_WIN}/Packages/User"
+else
+  echo "Sublime path:"
+  read SUBL_PATH
+  SUBL_USER="${SUBL_PATH}/Data/Packages/User"
+fi
+
+mkdir -p "${SUBL_USER}"
+cp -v ./conf/sublime3/* "${SUBL_USER}"/
