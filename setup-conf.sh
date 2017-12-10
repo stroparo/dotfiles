e89b9e204af61bb989bbbb9b24e32425fd717303
Cristian Stroparo <cstropz@gmail.com>
2017-12-10 16:16:21 -0200

    Updated

--- setup-conf.sh
+++ setup-conf.sh
@@ -4,7 +4,9 @@ cp -v ./conf/exrc ~/.exrc
 cp -v ./conf/vimrc ~/.vimrc
 cp -v ./conf/sshconfig ~/.ssh/config
 
-# Sublime Text:
+# ##############################################################################
+# Prep Sublime Text User PATH:
+
 SUBL_WIN='C:\Users\cr391577\AppData\Roaming\Sublime Text 3'
 if [[ "$(uname -a)" = *[Cc]ygwin* ]]; then
   if [ -d "`cygpath "${SUBL_WIN}"`" ] ; then
@@ -18,5 +20,10 @@ elif [[ "$(uname -a)" = *[Ll]inux* ]] ; then
   SUBL_USER="$HOME/.config/sublime-text-3/Packages/User"
 fi
 
+# ##############################################################################
+# Sublime Text
+
 mkdir -p "${SUBL_USER}"
 cp -v ./conf/sublime3/* "${SUBL_USER}"/
+
+# ##############################################################################
