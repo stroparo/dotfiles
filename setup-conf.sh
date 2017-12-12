596112967ac5862399c91d0bd0a6ff46402627d5
Cristian Stroparo <cstropz@gmail.com>
2017-12-12 06:58:03 -0600

    Updated install for subl in cygwin /cygdrive/c/opt/subl as well

--- setup-conf.sh
+++ setup-conf.sh
@@ -15,51 +15,52 @@ cp -v ./conf/vimrc ~/.vimrc
 cp -v ./conf/sshconfig ~/.ssh/config
 
 # ##############################################################################
+# GUI envs from this point on...
+
+_is_gui_env || return 0 || exit 0
+
+# ##############################################################################
 # Prep Sublime Text User PATH
 
-if _is_gui_env ; then
+SUBL_WIN='C:\Users\cr391577\AppData\Roaming\Sublime Text 3'
 
-  SUBL_WIN='C:\Users\cr391577\AppData\Roaming\Sublime Text 3'
-
-  if [[ "$(uname -a)" = *[Cc]ygwin* ]]; then
-    if [ -d "`cygpath "${SUBL_WIN}"`" ] ; then
-      SUBL_USER="${SUBL_WIN}/Packages/User"
-    else
-      echo "Sublime path:"
-      read SUBL_PATH
-      SUBL_USER="${SUBL_PATH}/Data/Packages/User"
-    fi
-  elif [[ "$(uname -a)" = *[Ll]inux* ]] ; then
-    SUBL_USER="$HOME/.config/sublime-text-3/Packages/User"
+if [[ "$(uname -a)" = *[Cc]ygwin* ]]; then
+  if [ -d "`cygpath "${SUBL_WIN}"`" ] ; then
+    SUBL_USER="${SUBL_WIN}/Packages/User"
+  elif [ -d "/cygdrive/c/opt/subl" ] ; then
+    SUBL_USER="/cygdrive/c/opt/subl/Data/Packages/User"
+  else
+    echo "Sublime path:"
+    read SUBL_PATH
+    SUBL_USER="${SUBL_PATH}/Data/Packages/User"
   fi
+elif [[ "$(uname -a)" = *[Ll]inux* ]] ; then
+  SUBL_USER="$HOME/.config/sublime-text-3/Packages/User"
 fi
 
 # ##############################################################################
 # Sublime Text
 
-if _is_gui_env ; then
+mkdir -p "${SUBL_USER}"
 
-  mkdir -p "${SUBL_USER}"
+if [ -f "${SUBL_USER}/Preferences.sublime-settings" ] ; then
 
-  if [ -f "${SUBL_USER}/Preferences.sublime-settings" ] ; then
+  # Do not overwrite settings
 
-    # Do not overwrite settings
+  files="$(ls -1d ./conf/sublime3/* \
+    | grep -F -v Preferences.sublime-settings \
+    | sed "s/^/'/" \
+    | sed "s/$/'/" \
+    | tr '\n' ' ')"
 
-    files="$(ls -1d ./conf/sublime3/* \
-      | grep -F -v Preferences.sublime-settings \
-      | sed "s/^/'/" \
-      | sed "s/$/'/" \
-      | tr '\n' ' ')"
+else
 
-  else
-
-    files="$(ls -1d ./conf/sublime3/* \
-      | sed "s/^/'/" \
-      | sed "s/$/'/" \
-      | tr '\n' ' ')"
-  fi
-
-  eval cp -v "${files}" "${SUBL_USER}"/
+  files="$(ls -1d ./conf/sublime3/* \
+    | sed "s/^/'/" \
+    | sed "s/$/'/" \
+    | tr '\n' ' ')"
 fi
+
+eval cp -v "${files}" "${SUBL_USER}"/
 
 # ##############################################################################
