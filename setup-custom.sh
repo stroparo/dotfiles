26fc3d3a65dbf5f2703b052ca0b793ff309930d5
Cristian Stroparo <cstropz@gmail.com>
2018-01-07 17:26:37 -0200

    setup scripts now use either curl or wget

--- setup-custom.sh
+++ setup-custom.sh
@@ -3,41 +3,53 @@
 SCRIPT_DIR="${0%/*}"
 SCRIPT_DIR="${SCRIPT_DIR:-$(pwd)}"
 
+# #############################################################################
+# Globals
+
 INSTALL_APT="$SCRIPT_DIR/custom/install-apt-packages.sh"
 INSTALL_PPA="$SCRIPT_DIR/custom/install-ppa-packages.sh"
 INSTALL_YUM="$SCRIPT_DIR/custom/install-yum-packages.sh"
 MASTER_URL=https://raw.githubusercontent.com/stroparo/dotfiles/master
 SETUP_URL=https://raw.githubusercontent.com/stroparo/dotfiles/master/setup.sh
 
-! which curl &> /dev/null \
-  && echo "FATAL: curl missing" 1>&2 \
-  && exit 1
+if which curl >/dev/null 2>&1 ; then
+  export DLPROG=curl
+  export DLOPT='-LSfs'
+elif which wget >/dev/null 2>&1 ; then
+  export DLPROG=wget
+  export DLOPT='-O -'
+else
+  echo "FATAL: curl and wget missing" 1>&2
+  exit 1
+fi
+
+# #############################################################################
 
 # Install custom package selections:
 if grep -E -i -q 'debian|ubuntu' /etc/*release* 2>/dev/null ; then
-  if [ -f "$INSTALL_APT" ] ; then
-    "$INSTALL_APT"
+  if [ -f "${INSTALL_APT}" ] ; then
+    "${INSTALL_APT}"
   else
-    sh -c "$(curl -Lf "$MASTER_URL/custom/install-apt-packages.sh")"
+    sh -c "$(${DLPROG} ${DLOPT} "${MASTER_URL}/custom/install-apt-packages.sh")"
   fi
 
 fi
 
 # Install PPA packages:
 if grep -E -i -q ubuntu /etc/*release* 2>/dev/null ; then
-  if [ -f "$INSTALL_PPA" ] ; then
-    "$INSTALL_PPA"
+  if [ -f "${INSTALL_PPA}" ] ; then
+    "${INSTALL_PPA}"
   else
-    sh -c "$(curl -Lf "$MASTER_URL/custom/install-ppa-packages.sh")"
+    sh -c "$(${DLPROG} ${DLOPT} "${MASTER_URL}/custom/install-ppa-packages.sh")"
   fi
 fi
 
 # Install YUM packages:
 if grep -E -i -q 'centos|oracle|red ?hat' /etc/*release* 2>/dev/null ; then
-  if [ -f "$INSTALL_YUM" ] ; then
-    "$INSTALL_YUM"
+  if [ -f "${INSTALL_YUM}" ] ; then
+    "${INSTALL_YUM}"
   else
-    sh -c "$(curl -Lf "$MASTER_URL/custom/install-yum-packages.sh")"
+    sh -c "$(${DLPROG} ${DLOPT} "${MASTER_URL}/custom/install-yum-packages.sh")"
   fi
 fi
 
@@ -45,20 +57,20 @@ fi
 if [ -f ./setup.sh ] ; then
   ./setup.sh
 else
-  sh -c "$(curl -LSfs "$SETUP_URL")"
+  sh -c "$(${DLPROG} ${DLOPT} "${SETUP_URL}")"
 fi
 
 # Make the workspace directory:
 export DEV=~/workspace
-mkdir -p "$DEV"
-ls -d -l "$DEV"
+mkdir -p "${DEV}"
+ls -d -l "${DEV}"
 
 # Echo repo cloning loop command:
 cat <<EOF
 
 # Clone your git repositories by running this sequence:
 
-cd '$DEV'
+cd '${DEV}'
 while true ; do
   echo "Type repo URL or Ctrl-C to finish: "
   read repo
--- setup.sh
+++ setup.sh
@@ -1,26 +1,46 @@
-#/usr/bin/env sh
+#/usr/bin/env bash
 
-! which curl &> /dev/null \
-  && echo "FATAL: curl missing" 1>&2 \
-  && exit 1
+# #############################################################################
+# Globals
+
+export DSEXTRAS_GIT="https://github.com/stroparo/ds-extras.git"
+
+export OHMYZSH_URL="https://raw.github.com/robbyrussell/\
+oh-my-zsh/master/tools/install.sh"
+
+export SETUP_URL="https://raw.githubusercontent.com/stroparo/ds/\
+master/setup.sh"
+
+if which curl >/dev/null 2>&1 ; then
+  export DLPROG=curl
+  export DLOPT='-LSfs'
+elif which wget >/dev/null 2>&1 ; then
+  export DLPROG=wget
+  export DLOPT='-O -'
+else
+  echo "FATAL: curl and wget missing" 1>&2
+  exit 1
+fi
+
+# #############################################################################
 
 # Daily Shells
-sh -c "$(curl -LSfs https://raw.githubusercontent.com/stroparo/ds/master/setup.sh)"
+sh -c "$(${DLPROG} ${DLOPT} "${SETUP_URL}")"
 
 # Daily Shells Extras
-git clone https://github.com/stroparo/ds-extras.git ~/.ds-extras \
+git clone "${DSEXTRAS_GIT}" ~/.ds-extras \
   && (cd ~/.ds-extras && . ./overlay.sh) \
   && rm -rf ~/.ds-extras
 
 # OhMyZsh
-sh -c "$(curl -LSfs https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(${DLPROG} ${DLOPT} "${OHMYZSH_URL}")"
 (. ~/.ds/ds.sh && [ -n "$DS_LOADED" ] && installohmyzsh.sh)
 
 # SSH key
 if [[ $USER != root ]] && [ ! -e ~/.ssh/id_rsa ] ; then
-  
+
   mkdir ~/.ssh
-  
+
   ssh-keygen -t rsa -C "$USER@$(hostname)" \
     && chmod 700 ~/.ssh/id_rsa \
     && ls -l ~/.ssh/id_rsa
