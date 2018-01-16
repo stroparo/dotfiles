8f878e16998e2955ca9dab398d71b3658ccede01
Cristian Stroparo <cstropz@gmail.com>
2018-01-16 15:18:01 -0600

    Enable dotify.sh

--- scripts/dotify.sh
+++ scripts/dotify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 for conf_item in $(ls -d ./conf/*) ; do
-  echo cp -a -v "${conf_item}" "${HOME}/.${conf_item#./conf/}"
+  cp -a -v "${conf_item}" "${HOME}/.${conf_item#./conf/}"
 done
