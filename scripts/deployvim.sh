e7d1019b6ba037204c19c44b1fd852356ec4311b
Cristian Stroparo <cstropz@gmail.com>
2018-01-29 22:52:18 -0200

    Remove rm's verbosity

--- scripts/deployvim.sh
+++ scripts/deployvim.sh
@@ -5,4 +5,4 @@ mkdir -p "$HOME"/.vim/undodir
 
 git clone https://github.com/jnurmine/Zenburn.git "$HOME"/zenburn \
   && mv -f -v "$HOME"/zenburn/colors/zenburn.vim "$HOME"/.vim/colors/ \
-  && rm -f -r -v "$HOME"/zenburn
+  && rm -f -r "$HOME"/zenburn
