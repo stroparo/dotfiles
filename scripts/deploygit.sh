c8eea3801ab3e2728c23561e47806154d506e621
Cristian Stroparo <cstropz@gmail.com>
2018-01-30 21:16:54 -0200

    Skip deploygit if git unavailable

--- scripts/deploygit.sh
+++ scripts/deploygit.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if ! which git >/dev/null 2>&1 ; then
+  echo "deploygit: SKIP: no git available" 1>&2
+  exit
+fi
+
 git config --global core.excludesfile ~/.gitignore_global
