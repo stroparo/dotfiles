1c4804499ecfde2e9de39ec2d50ff789040233f3
Cristian Stroparo <cstropz@gmail.com>
2018-01-29 22:51:36 -0200

    Fix vim zenburn colorscheme deployment

--- scripts/deployvim.sh
+++ scripts/deployvim.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+mkdir -p "$HOME"/.vim/colors
 mkdir -p "$HOME"/.vim/undodir
 
-git clone https://github.com/jnurmine/Zenburn.git ~/.vim/colors/zenburn
+git clone https://github.com/jnurmine/Zenburn.git "$HOME"/zenburn \
+  && mv -f -v "$HOME"/zenburn/colors/zenburn.vim "$HOME"/.vim/colors/ \
+  && rm -f -r -v "$HOME"/zenburn
