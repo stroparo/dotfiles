aea5eb03babacc8a8ab2efe61c3eb63635b4a43f
Cristian Stroparo <cstropz@gmail.com>
2018-01-30 21:38:38 -0200

    Info msg

--- scripts/dotify.sh
+++ scripts/dotify.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo
+echo "==> Dotifying all files in conf/ to the home directory..."
+
 for conf_item in $(ls -d ./conf/*) ; do
   cp -a -v "${conf_item}" "${HOME}/.${conf_item#./conf/}"
 done
