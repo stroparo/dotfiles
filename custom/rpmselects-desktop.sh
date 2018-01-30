210df9eca4df5ba716a21bba6ee2ca1b51abda4e
Cristian Stroparo <cstropz@gmail.com>
2018-01-30 21:00:09 -0200

    rpmselects-desktop.sh made succint

--- custom/rpmselects-desktop.sh
+++ custom/rpmselects-desktop.sh
@@ -33,44 +33,7 @@ echo ${BASH_VERSION:+-e} "\n==> Upgrade all packages? [y/N]\c" ; read answer
 [[ $answer = y ]] && sudo $RPMPROG update
 
 # #############################################################################
-# Desktop applications, interactive
+# Install
 
-echo ${BASH_VERSION:+-e} "\n==> Office? [y/N]\c" ; read answer
-[[ $answer = y ]] && sudo $RPMPROG install libreoffice-calc
-
-echo ${BASH_VERSION:+-e} "\n==> Other packages? [y/N]\c" ; read answer
-if [[ $answer = y ]] ; then
-
-  PACKAGES="autorenamer
-gigolo
-gnome-shell-pomodoro
-mpv
-parole
-smplayer
-unison unison-gtk
-usb-creator-gtk
-"
-
-  for package in $(echo $PACKAGES) ; do
-    echo ${BASH_VERSION:+-e} "\n==> $RPMPROG install '$package'? [Y/n]\c" ; read answer
-    if [[ $answer != n ]] ; then
-      sudo $RPMPROG install -y $package
-    fi
-  done
-
-fi
-
-# #############################################################################
-# Silver Searcher Ag
-# Source: https://github.com/ggreer/the_silver_searcher
-
-if ! ag --version ; then
-  if ! grep -i -q 'fedora' /etc/*release* ; then
-    sudo $RPMPROG install -y epel-release.noarch
-  fi
-  sudo $RPMPROG install -y the_silver_searcher
-fi
-
-# #############################################################################
-# Cleanup
+sudo $RPMPROG install gigolo guake libreoffice-calc meld parole
 
