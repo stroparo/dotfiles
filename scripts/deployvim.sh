7caf6e4ca5d02a8adec485f82e005e3c856c1dc8
Cristian Stroparo <cstropz@gmail.com>
2018-01-30 09:38:03 -0600

    Fix

--- scripts/deployvim.sh
+++ scripts/deployvim.sh
@@ -3,7 +3,7 @@
 mkdir -p "$HOME"/.vim/colors
 mkdir -p "$HOME"/.vim/undodir
 
-if [ ! -d "$HOME"/.vim/colors/zenburn.vim ] ; then
+if [ ! -e "$HOME"/.vim/colors/zenburn.vim ] ; then
   git clone https://github.com/jnurmine/Zenburn.git "$HOME"/zenburn \
     && mv -f -v "$HOME"/zenburn/colors/zenburn.vim "$HOME"/.vim/colors/ \
     && rm -f -r "$HOME"/zenburn
