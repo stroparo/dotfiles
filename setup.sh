9c7b8c81e267d3a63e86b972a592a8bd44da8aeb
Cristian Stroparo <cstropz@gmail.com>
2018-01-31 07:22:57 -0600

    Updated setup.sh only run setupbox.sh by using -b option

--- setup.sh
+++ setup.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+# Run this script from its directory otherwise it will not find
+#  the ./scripts/ directory and will self provision.
+
 # #############################################################################
 # Self provisioning
 
 if [ ! -d ./scripts ] ; then
-  
+
   curl -LSfs -o "$HOME"/.dotfiles.zip \
     https://github.com/stroparo/dotfiles/archive/master.zip
-  
+
   unzip -o "$HOME"/.dotfiles.zip -d "$HOME" \
     && (cd "$HOME"/dotfiles-master \
     && [ "$PWD" = "$HOME"/dotfiles-master ] \
     && ./setup.sh) \
     || exit $?
-  
+
   rm -f -r "$HOME"/dotfiles-master
   exit
 fi
@@ -21,19 +24,30 @@ fi
 # #############################################################################
 # Options
 
-export OVERRIDE_SUBL_PREFS=false
+OVERRIDE_SUBL_PREFS=false
+DO_BOX=false
+FULL=false
 
-if [ "$1" = '-f' ] ; then
-  export OVERRIDE_SUBL_PREFS=true
-  shift
-fi
+# Options:
+OPTIND=1
+while getopts ':bf' option ; do
+    case "${option}" in
+        b) DO_BOX=true;;
+        f)
+          FULL=true
+          OVERRIDE_SUBL_PREFS=true;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+export OVERRIDE_SUBL_PREFS
 
 # #############################################################################
 # Configurations
 
 ./scripts/dotify.sh
 ./setupaliases.sh
-./setupbox.sh
+${DO_BOX:-false} && ./setupbox.sh
 
 for deploy in `ls ./scripts/deploy*sh` ; do
   "$deploy"
