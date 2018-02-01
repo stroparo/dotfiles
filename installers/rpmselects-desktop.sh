193929d2ab59a51965d8ee6e7beea9810da18540
Cristian Stroparo <cstropz@gmail.com>
2018-02-01 18:31:26 -0200

    Fedora 26 & 27 XFCE Whisker Menu installations

--- installers/rpmselects-desktop.sh
+++ installers/rpmselects-desktop.sh
@@ -9,7 +9,7 @@ export RPMPROG=yum; which dnf >/dev/null 2>&1 && export RPMPROG=dnf
 # Check OS
 
 if ! egrep -i -q 'cent ?os|fedora|oracle|red ?hat' /etc/*release* ; then
-  echo "FATAL: Only Red Hat based distros are allowed to call this script ($0)" 1>&2
+  echo "FATAL: Only Red Hat based distros are supported" 1>&2
   exit 1
 fi
 
@@ -17,7 +17,7 @@ fi
 # Sudo check
 
 if ! which sudo ; then
-  echo "FATAL: Please log in as root, install and then configure sudo for your user first.." 1>&2
+  echo "FATAL: Please setup sudo for your user first.." 1>&2
   cat "Suggested commands:" <<EOF
 su -
 $RPMPROG install sudo
@@ -38,4 +38,24 @@ echo ${BASH_VERSION:+-e} "\n==> Upgrade all packages? [y/N]\c" ; read answer
 sudo $RPMPROG install gigolo guake meld parole
 sudo $RPMPROG install libreoffice-calc
 sudo $RPMPROG install xfce4-whiskermenu-plugin
+
+# #############################################################################
+# Fedora 26 & 27
+
+if egrep -i -q 'fedora 2[67]' /etc/*release* ; then
+
+  fedora_version=$(egrep -i -o 'fedora 2[67]' /etc/*release* \
+    | head -1 \
+	| awk '{ print $2; }')
+
+  # XFCE Whisker Menu:
+  sudo $RPMPROG remove xfce4-whiskermenu-plugin
+  sudo curl -o /etc/yum.repos.d/home:gottcode.repo \
+    "http://download.opensuse.org/repositories/home:\
+/gottcode/Fedora_${fedora_version}/home:\
+gottcode.repo"
+  sudo $RPMPROG install xfce4-whiskermenu-plugin
+fi
+
+# #############################################################################
 
