6579f90703bc41c13dff05bfd3e865dfc1095eeb
Cristian Stroparo <cstropz@gmail.com>
2018-02-14 13:47:05 -0600

    Updated setupxfce with fonts

--- installers/setupxfce.sh
+++ installers/setupxfce.sh
@@ -3,4 +3,5 @@
 if egrep -i -q '(centos|fedora|oracle|red *hat).* 6' /etc/*release*
   sudo yum groupinstall desktop "general purpose desktop" "x window system"
   sudo yum groupinstall xfce
+  sudo yum -y install xorg-x11-fonts-Type1 xorg-x11-fonts-misc
 fi
