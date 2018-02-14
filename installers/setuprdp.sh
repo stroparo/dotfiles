6a14510a06dff74a785887e91983ede383d78583
Cristian Stroparo <cstropz@gmail.com>
2018-02-14 17:57:52 -0600

    Updated rdp setup override of Xclients for xfce4 for el6

--- installers/setuprdp.sh
+++ installers/setuprdp.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-if egrep -i -q '(centos|fedora|oracle|red *hat).* 6' /etc/*release*
+if egrep -i -q 'centos|fedora|oracle|red *hat' /etc/*release* ; then
   sudo yum -y install xrdp tigervnc-server
+fi
+
+if egrep -i -q '(centos|fedora|oracle|red *hat).* 6' /etc/*release*
+  echo ${BASH_VERSION:+-e} "Force Xclients to startxfce4? [y/N]\c"
+  read ans
+  if [ "$ans" = y ] ; then
+    sudo mv -v /etc/X11/xinit/Xclients /etc/X11/xinit/Xclients.bak.$(date '+%Y-%m-%d-%OH%OM%OS')
+    cat <<'EOF' | sudo tee /etc/X11/xinit/Xclients
+#!/usr/bin/env bash
+exec $(which startxfce4)
+EOF
+  fi
 fi
