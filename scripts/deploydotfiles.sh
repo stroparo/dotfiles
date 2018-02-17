48518b76ed928fafc8071d19774a1b938458bdfa
Cristian Stroparo <cstropz@gmail.com>
2018-02-16 22:11:24 -0200

    Fix deploydotfiles and Thunar custom action to open with sublime

--- scripts/deploydotfiles.sh
+++ scripts/deploydotfiles.sh
@@ -2,9 +2,23 @@
 
 # Cristian Stroparo's dotfiles - https://github.com/stroparo/dotfiles
 
+PROGNAME=deploydotfiles.sh
+
 echo
 echo "==> Dotifying all files in conf/ to the home directory..."
 
 for conf_item in $(ls -d ./conf/*) ; do
-  cp -a -v "${conf_item}" "${HOME}/.${conf_item#./conf/}"
+  destname="${HOME}/.${conf_item#./conf/}"
+  if [ -d "$conf_item" ] ; then
+    if [ ! -d "$destname" ] ; then
+      mkdir "$destname"
+      if [ ! -d "$destname" ] ; then
+        echo "$PROGNAME: SKIP: could not mkdir '$destname'." 1>&2
+        continue
+      fi
+    fi
+    cp -f -R -v "${conf_item}"/* "$destname"/
+  else
+    cp -f -R -v "${conf_item}" "$destname"
+  fi
 done
