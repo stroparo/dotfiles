0d2f971252ccd3f91ca2cdac92dde5e27b47ea62
Cristian Stroparo <cstropz@gmail.com>
2018-02-18 12:55:08 -0300

    Updated copy_path_to_clipboard.py command now with no line number, just the current filename

---
+++ sublime3/copy_path_to_clipboard.py
@@ -0,0 +1,14 @@
+import sublime
+import sublime_plugin
+
+
+class CopyPathToClipboard(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        sublime.set_clipboard(self.view.file_name())
+
+    # # With the line number, in Sublime notation:
+    # def run(self, edit):
+    #     line_number, column = self.view.rowcol(self.view.sel()[0].begin())
+    #     line_number += 1
+    #     sublime.set_clipboard(self.view.file_name() + ':' + str(line_number))
