86b4176fb191a95d238e308f4d065d0fac043d55
Cristian Stroparo <cstropz@gmail.com>
2018-02-19 14:57:38 -0600

    +setupsubl.sh

---
+++ installers/setupsubl.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Daily Shells Extras extensions
+# More instructions and licensing at:
+# https://github.com/stroparo/ds-extras
+
+echo ${BASH_VERSION:+-e} '\n\n==> Installing subl...' 1>&2
+
+# #############################################################################
+# Globals
+
+SUBL_URL_LINUX='https://download.sublimetext.com/sublime_text_3_build_3143_x64.tar.bz2'
+
+# #############################################################################
+# Checks
+
+# Check for idempotency
+if type subl >/dev/null 2>&1 ; then
+  echo "SKIP: Already installed." 1>&2
+  exit
+fi
+
+# #############################################################################
+# Linux
+
+if egrep -i -q 'linux' ; then
+
+  mkdir -p "$HOME/opt"
+  cd "$HOME/opt"
+  curl -k -LSf -o ./subl3.tar.bz2 "$SUBL_URL_LINUX"
+  tar xjvf ./subl3.tar.bz2
+  ln -s -v "$PWD/sublime_text_3" ./subl
+
+# #############################################################################
+# Cygwin
+
+elif [[ "$(uname -a)" = *[Cc]ygwin* ]] ; then
+
+  :
+  # TODO
+  # wget 'https://subl.io/download/windows'
+  # mv windows sublsetup.exe
+  # chmod u+x sublsetup.exe && ./sublsetup.exe && rm -f ./sublsetup.exe
+
+# #############################################################################
+
+else
+  echo "FATAL: OS not handled." 1>&2
+  exit 1
+fi
