73063857327ecdd62ba28d538219d15d616ac334
Cristian Stroparo <cstropz@gmail.com>
2018-02-28 20:56:50 -0300

    Updated setupsubl to catter for distro-package install as default (portable still handled via -p option)

--- installers/setupsubl.sh
+++ installers/setupsubl.sh
@@ -1,61 +1,91 @@
 #!/usr/bin/env bash
 
-# Daily Shells Extras extensions
-# More instructions and licensing at:
-# https://github.com/stroparo/ds-extras
+# Cristian Stroparo's dotfiles - https://github.com/stroparo/dotfiles
 
-echo ${BASH_VERSION:+-e} '\n\n==> Installing subl...' 1>&2
+PROGNAME="setupsubl.sh"
+USAGE="$PROGNAME [-d opt dir (effect with -p only)] [-h] [-p]"
+
+echo ${BASH_VERSION:+-e} '\n\n==> Installing sublime-text...' 1>&2
 
 # #############################################################################
 # Globals
 
-SUBL_DIR="$HOME/opt"
-SUBL_URL_LINUX='https://download.sublimetext.com/sublime_text_3_build_3143_x64.tar.bz2'
-SUBL_URL_WINDOWS='https://download.sublimetext.com/Sublime%20Text%20Build%203143%20x64.zip'
+DO_PORTABLE=false
 
-# #############################################################################
-# Checks
+# Deb-based package
+SUBL_APT_KEY="https://download.sublimetext.com/sublimehq-pub.gpg"
+SUBL_APT_PKG="sublime-text"
+SUBL_APT_REPO="deb https://download.sublimetext.com/ apt/stable/"
 
-# Check for idempotency
-if type subl >/dev/null 2>&1 ; then
-  echo "SKIP: Already installed." 1>&2
-  exit
-fi
+# EL-based package
+SUBL_RPM_KEY="https://download.sublimetext.com/sublimehq-rpm-pub.gpg"
+SUBL_RPM_PKG="sublime-text"
+SUBL_RPM_REPO="https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo"
+
+SUBL_OPT_DIR="$HOME/opt"
+SUBL_PORTABLE_LINUX='https://download.sublimetext.com/sublime_text_3_build_3143_x64.tar.bz2'
+SUBL_PORTABLE_WINDOWS='https://download.sublimetext.com/Sublime%20Text%20Build%203143%20x64.zip'
 
 # #############################################################################
 # Options
 
-# Options:
 OPTIND=1
-while getopts ':d:' option ; do
+while getopts ':d:hp' option ; do
   case "${option}" in
-    d) SUBL_DIR="${OPTARG}";;
+    d) SUBL_OPT_DIR="${OPTARG}";;
     h) echo "$USAGE"; exit;;
+    p) DO_PORTABLE=true;;
   esac
 done
 shift "$((OPTIND-1))"
 
+export DO_PORTABLE
+export SUBL_OPT_DIR
+
 # #############################################################################
-# Linux
+# Distribution-wise package
 
-if egrep -i -q 'linux' /etc/*release* ; then
+if ! ${DO_PORTABLE:-false} ; then
 
-  mkdir -p "$SUBL_DIR"
-  cd "$SUBL_DIR"
-  curl -k -L -o ./subl3.tar.bz2 "$SUBL_URL_LINUX"
+  if egrep -i -q 'debian|ubuntu' /etc/*release* ; then
+    curl -LSf "$SUBL_APT_KEY" | sudo apt-key add -
+    sudo apt-get install apt-transport-https
+    echo "$SUBL_APT_REPO" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    sudo apt-get update
+    sudo apt-get install "$SUBL_APT_PKG"
+  elif egrep -i -q 'centos|fedora|oracle|red *hat' /etc/*release* ; then
+    if which dnf 2>/dev/null ; then
+      sudo rpm -v --import "$SUBL_RPM_KEY"
+      sudo dnf config-manager --add-repo "$SUBL_RPM_REPO"
+      sudo dnf install "$SUBL_RPM_PKG"
+    else
+      sudo rpm -v --import "$SUBL_RPM_KEY"
+      sudo yum-config-manager --add-repo "$SUBL_RPM_REPO"
+      sudo yum install "$SUBL_RPM_PKG"
+    fi
+  fi
+
+# #############################################################################
+# Portable for Linux
+
+elif egrep -i -q 'linux' /etc/*release* ; then
+
+  mkdir -p "$SUBL_OPT_DIR"
+  cd "$SUBL_OPT_DIR"
+  curl -k -L -o ./subl3.tar.bz2 "$SUBL_PORTABLE_LINUX"
   sudo tar xjvf ./subl3.tar.bz2
   sudo ln -s -v ./sublime_text_3 ./subl # directory
   sudo ln -s -v sublime_text ./subl/subl # binary
 
 # #############################################################################
-# Cygwin
+# Portable for Windows
 
 elif [[ "$(uname -a)" = *[Cc]ygwin* ]] ; then
 
-  SUBL_DIR="$(cygpath 'C:\opt')"
-  mkdir -p "$SUBL_DIR"
-  cd "$SUBL_DIR"
-  curl -k -L -o ./subl3.zip "$SUBL_URL_WINDOWS"
+  SUBL_OPT_DIR="$(cygpath 'C:\opt')"
+  mkdir -p "$SUBL_OPT_DIR"
+  cd "$SUBL_OPT_DIR"
+  curl -k -L -o ./subl3.zip "$SUBL_PORTABLE_WINDOWS"
   unzip ./subl3.zip
 
 # #############################################################################
