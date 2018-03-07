449c34c1c4c13f98b2f9b91303d881250bfe091a
Cristian Stroparo <cstropz@gmail.com>
2018-03-07 11:45:37 -0600

    More portable

--- installers/setupfonts-el7.sh
+++ installers/setupfonts-el7.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -20,18 +20,18 @@ fi
 
 # set up nux-dextop repo to install font packages. skip if this repo had already set up.
 # can be done by either rpm or yum app.
-# /usr/bin/sudo /usr/bin/rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
-/usr/bin/sudo /usr/bin/yum localinstall http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+# sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+sudo yum localinstall http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 
 # disable nux-dextop by default and only enable it as needed as part of running yum.
 # skip this step to make all packages in nux-dextop available at all time.
-/usr/bin/sudo /usr/bin/sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/nux-dextop.repo
+sudo sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/nux-dextop.repo
 
 # install the magical packages that does font look nice.
-/usr/bin/sudo /usr/bin/yum --enablerepo=nux-dextop install fontconfig-infinality cairo libXft freetype-infinality
+sudo yum --enablerepo=nux-dextop install fontconfig-infinality cairo libXft freetype-infinality
 
 # a good .fonts.conf file. not sure if necessary.
-/usr/bin/cat <<EOF > ~/.fonts.conf
+cat <<EOF > ~/.fonts.conf
 <?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
 <fontconfig>
@@ -69,5 +69,5 @@ fi
 EOF
 
 # done.
-/usr/bin/echo
-/usr/bin/echo 'log out and log back in for clear looking fonts.'
+echo
+echo 'log out and log back in for clear looking fonts.'
