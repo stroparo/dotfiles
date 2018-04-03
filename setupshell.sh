951ef26018002b3388f21df3f4f6efdc8fed3b18
Cristian Stroparo <cstropz@gmail.com>
2018-04-03 12:35:38 -0500

    Updated

--- setupshell.sh
+++ setupshell.sh
@@ -29,26 +29,29 @@ fi
 
 # Daily Shells
 bash -c "$(${DLPROG} ${DLOPT} "${DS_SETUP_URL}")"
-
-# Daily Shells Extras
-rm -rf ~/.ds-extras >/dev/null 2>&1
-git clone --depth 1 "${DSEXTRAS_GIT}" ~/.ds-extras \
-  && (cd ~/.ds-extras && . ./overlay.sh) \
-  && rm -rf ~/.ds-extras
-
-# Load Daily Shells
 . ~/.ds/ds.sh
 if ! ${DS_LOADED:-false} ; then
   echo "${PROGNAME:+${PROGNAME}: }FATAL: Could not load Daily Shells." 1>&2
   exit 1
 fi
 
+"dsplugin.sh" "stroparo/ds-extras"
+. ~/.ds/ds.sh
+
+echo
+echo "################################################################################"
 echo
 echo "==> After installing oh-my-zsh, it will change"
 echo "    the default shell to zsh and log into it."
 echo "    IF THAT IS THE CASE (like the prompt stopped"
 echo "    and nothing else happened), then exit or ctrl+d"
 echo "    for this sequence to continue."
+echo
 "$PROGDIR/installers/setupohmyzsh.sh"
 
-sshkeygenrsa.sh
+echo
+echo "################################################################################"
+echo
+echo "==> sshkeygenrsa.sh"
+echo
+"sshkeygenrsa.sh"
