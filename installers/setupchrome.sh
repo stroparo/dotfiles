b8664d3ee5c6ed6ee75eca9f5681226701a5b287
Cristian Stroparo <cstropz@gmail.com>
2018-04-03 19:27:19 -0300

    Updated setupchrome.sh implemented debian/ubuntu

--- installers/setupchrome.sh
+++ installers/setupchrome.sh
@@ -28,6 +28,8 @@ EOF
 
 elif egrep -i -q 'debian|ubuntu' /etc/*release ; then
 
-  echo "SKIP: Debian/Ubuntu to be implemented" 1>&2
-
+  curl -LSfs "https://dl-ssl.google.com/linux/linux_signing_key.pub" | sudo apt-key add -
+  sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+  sudo $APTPROG update
+  sudo $APTPROG install -y google-chrome-stable
 fi
