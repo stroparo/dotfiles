37ba664b2d9cea11829d7347dc179793430c4ede
Cristian Stroparo <cstropz@gmail.com>
2018-04-11 13:05:54 -0500

    Updated setuppython.sh pip.conf setup with format=columns

--- installers/setuppython.sh
+++ installers/setuppython.sh
@@ -119,6 +119,17 @@ sudo -H pip3 install --upgrade pip
 )
 
 # #############################################################################
+# pip.conf
+
+mkdir -p "$HOME/.config/pip" 2>/dev/null
+cp -v "$HOME/.config/pip/pip.conf" "$HOME/.config/pip/pip.conf.$(date '+%Y%m%d-%OH%OM%OS')"
+cat > "$HOME/.config/pip/pip.conf" <<EOF
+[list]
+format=columns
+EOF
+
+# #############################################################################
+# Checkpoint
 
 ${SYSTEM_ONLY:-false} && exit
 
