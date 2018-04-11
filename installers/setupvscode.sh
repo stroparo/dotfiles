72a3f25b1d6dccfb31b2cc40db13efbb3a03cb0d
Cristian Stroparo <cstropz@gmail.com>
2018-04-10 22:51:44 -0300

    Updated setupvscode.sh start firefox with download links only if xorg is running

--- installers/setupvscode.sh
+++ installers/setupvscode.sh
@@ -5,24 +5,40 @@
 # #############################################################################
 # Globals
 
-PROGNAME=setupvscode.sh
+PROGNAME="setupvscode.sh"
 
 # #############################################################################
 # Main
 
 echo ${BASH_VERSION:+-e} "\n==> Visual Studio Code setup guide\n"
 
-echo ${BASH_VERSION:+-e} "\n==> Opening download and gist links...\n"
-firefox \
-  'https://code.visualstudio.com/download' \
-  'https://marketplace.visualstudio.com/items?itemName=nonoroazoro.syncing' \
-  'https://gist.github.com/stroparo/5ae1e0c8fe2e7a5401a9f52b3aaec9f1' \
-  'https://github.com/viatsko/awesome-vscode' \
-  &
+cat <<'EOF'
+
+Links:
+https://code.visualstudio.com/download
+https://marketplace.visualstudio.com/items?itemName=nonoroazoro.syncing
+https://gist.github.com/stroparo/5ae1e0c8fe2e7a5401a9f52b3aaec9f1
+https://github.com/viatsko/awesome-vscode
+EOF
+
+# #############################################################################
+# Open links
+
+if (ps -ef | grep -i -q xorg) && which firefox >/dev/null 2>&1 ; then
+
+  echo ${BASH_VERSION:+-e} "\n==> Opening download and gist links...\n"
+
+  firefox \
+    'https://code.visualstudio.com/download' \
+    'https://marketplace.visualstudio.com/items?itemName=nonoroazoro.syncing' \
+    'https://gist.github.com/stroparo/5ae1e0c8fe2e7a5401a9f52b3aaec9f1' \
+    'https://github.com/viatsko/awesome-vscode' \
+    &
+fi
 disown
 
 # #############################################################################
-# Tips
+# Tips on extensions
 
 cat <<EOF
 
@@ -38,3 +54,6 @@ Light themes:
 Quiet Light
 Solarized Light
 EOF
+
+# #############################################################################
+
