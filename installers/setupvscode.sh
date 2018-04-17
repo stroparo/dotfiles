395ab2b3b4388ef661926cbb5a95c64051f21b5f
Cristian Stroparo <cristian.stroparo@mastercard.com>
2018-04-17 07:40:52 -0500

    Updated

--- installers/setupvscode.sh
+++ installers/setupvscode.sh
@@ -7,18 +7,20 @@
 
 PROGNAME="setupvscode.sh"
 
+LINKS="https://code.visualstudio.com/download
+https://marketplace.visualstudio.com/items?itemName=nonoroazoro.syncing
+https://gist.github.com/stroparo/5ae1e0c8fe2e7a5401a9f52b3aaec9f1
+https://github.com/viatsko/awesome-vscode"
+
 # #############################################################################
 # Main
 
 echo ${BASH_VERSION:+-e} "\n==> Visual Studio Code setup guide\n"
 
-cat <<'EOF'
+cat <<EOF
 
 Links:
-https://code.visualstudio.com/download
-https://marketplace.visualstudio.com/items?itemName=nonoroazoro.syncing
-https://gist.github.com/stroparo/5ae1e0c8fe2e7a5401a9f52b3aaec9f1
-https://github.com/viatsko/awesome-vscode
+$LINKS
 EOF
 
 # #############################################################################
@@ -27,15 +29,8 @@ EOF
 if (ps -ef | grep -i -q xorg) && which firefox >/dev/null 2>&1 ; then
 
   echo ${BASH_VERSION:+-e} "\n==> Opening download and gist links...\n"
-
-  firefox \
-    'https://code.visualstudio.com/download' \
-    'https://marketplace.visualstudio.com/items?itemName=nonoroazoro.syncing' \
-    'https://gist.github.com/stroparo/5ae1e0c8fe2e7a5401a9f52b3aaec9f1' \
-    'https://github.com/viatsko/awesome-vscode' \
-    &
+  eval firefox $LINKS & disown
 fi
-disown
 
 # #############################################################################
 # Tips on extensions
