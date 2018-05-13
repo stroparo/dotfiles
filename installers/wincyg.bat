fc97abeb768655814e57fec2572591882b4953f3
Cristian Stroparo <cstropz@gmail.com>
2018-05-13 23:54:23 +0000

    Updated wincyg.bat cygwin setup script

--- installers/wincyg.bat
+++ installers/wincyg.bat
@@ -4,7 +4,6 @@ CD %~dp0
 
 SETLOCAL
 SET CYGCATS=archive,base,utils
-SET CYGPKGS=%CYGPKGS%,mintty,bash,bashdb,bash-completion,checkbashisms,perl,python,python3,zsh,p7zip,unzip,zip,curl,git,git-completion,lftp,mosh,ping,rsync,the_silver_searcher,tmux,vim,vim-common,wget,whois,wput
 SET CYGSETUP=%CD%\setup-x86_64.exe
 SET SITE=http://mirrors.kernel.org/sourceware/cygwin/
 rem SET SITE=http://linorg.usp.br/cygwin/
@@ -21,7 +20,31 @@ exit 1
 :OKSETUP
 
 ECHO INSTALLING PACKAGES..
-%CYGSETUP% -q -v -a x86_64 -d -g -o -Y -O -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P %CYGPKGS% -C %CYGCATS%
+
+rem %CYGSETUP% -q -v -a x86_64 -d -g -o -Y -O -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -C %CYGCATS%
+
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P curl
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P rsync
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P the_silver_searcher
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P tmux
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P p7zip
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P unzip
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P zip
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P bashdb
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P bash-completion
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P checkbashisms
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P git
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P git-completion
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P perl
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P python
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P python3
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P zsh
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P lftp
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P mosh
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P ping
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P wget
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P whois
+%CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P wput
 
 ECHO.
 ECHO Cygwin installation updated
