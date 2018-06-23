f1df68b77c46e1730fcaa987e40b95ab1d2af20b
Cristian Stroparo <cstropz@gmail.com>
2018-06-23 10:21:41 -0300

    Refactor debselects.sh output

--- installers/debselects.sh
+++ installers/debselects.sh
@@ -6,19 +6,39 @@
 # Globals
 
 export APTPROG=apt-get; which apt >/dev/null 2>&1 && export APTPROG=apt
+export INSTPROG="$APTPROG"
 
 # #############################################################################
-# Check OS
+# Helpers
+
+_install_packages () {
+  for package in "$@" ; do
+    echo "Installing '$package'..."
+    if ! sudo $INSTPROG install -y "$package" >/dev/null 2>/tmp/pkg-install-${package}.log ; then
+      echo "${PROGNAME:+$PROGNAME: }WARN: There was an error installing package '$package' - see '/tmp/pkg-install-${package}.log'." 1>&2
+    fi
+  done
+}
+
+_print_bar () {
+  echo "################################################################################"
+}
+
+_print_header () {
+  _print_bar
+  echo "$@"
+  _print_bar
+}
+
+# #############################################################################
+# Checks
 
 if ! egrep -i -q 'debian|ubuntu' /etc/*release ; then
   echo "FATAL: Only Debian/Ubuntu based distros are allowed to call this script ($0)" 1>&2
   exit 1
 fi
 
-# #############################################################################
-# Sudo check
-
-if ! which sudo ; then
+if ! which sudo >/dev/null 2>&1 ; then
   echo "FATAL: Please log in as root, install and then configure sudo for your user first.." 1>&2
   cat "Suggested commands:" <<EOF
 su -
@@ -29,53 +49,52 @@ EOF
 fi
 
 # #############################################################################
-# Update
-
-echo ${BASH_VERSION:+-e} "\n\n==> Updating..."
-sudo $APTPROG update
-sudo $APTPROG upgrade -y
-
-# #############################################################################
 # Main
 
-echo ${BASH_VERSION:+-e} "\n\n==> Base packages..."
-sudo $APTPROG install -y curl lftp mosh net-tools rsync wget
-# sudo $APTPROG install -y gdebi-core
-sudo $APTPROG install -y less
-sudo $APTPROG install -y localepurge
-sudo $APTPROG install -y logrotate
-sudo $APTPROG install -y parted
-sudo $APTPROG install -y p7zip-full p7zip-rar
-sudo $APTPROG install -y secure-delete
-sudo $APTPROG install -y silversearcher-ag
-sudo $APTPROG install -y sqlite3 libdbd-sqlite3
-sudo $APTPROG install -y tmux
-sudo $APTPROG install -y unzip zip
-sudo $APTPROG install -y zsh
+_print_header "Debian selects"
 
-echo ${BASH_VERSION:+-e} "\n\n==> Devel packages..."
-sudo $APTPROG install -y exuberant-ctags
-sudo $APTPROG install -y httpie
-sudo $APTPROG install -y git tig
-sudo $APTPROG install -y jq
-sudo $APTPROG install -y perl libperl-dev
-# sudo $APTPROG install -y ruby ruby-dev ruby-full
+echo "Debian APT index update..."
+if ! sudo $APTPROG update >/dev/null 2>/tmp/apt-update-err.log ; then
+  echo "WARN: There was some failure during APT index update - see '/tmp/apt-update-err.log'." 1>&2
+fi
 
-echo ${BASH_VERSION:+-e} "\n\n==> Devel libs..."
-sudo $APTPROG install -y gettext
-sudo $APTPROG install -y imagemagick
-sudo $APTPROG install -y libsqlite3-0 libsqlite3-dev
-sudo $APTPROG install -y libssl-dev
-sudo $APTPROG install -y zlib1g zlib1g-dev
+echo "Debian base packages..."
+_install_packages curl lftp mosh net-tools rsync wget
+# _install_packages gdebi-core
+_install_packages less
+_install_packages localepurge
+_install_packages logrotate
+_install_packages parted
+_install_packages p7zip-full p7zip-rar
+_install_packages secure-delete
+_install_packages silversearcher-ag
+_install_packages sqlite3 libdbd-sqlite3
+_install_packages tmux
+_install_packages unzip zip
+_install_packages zsh
 
-echo ${BASH_VERSION:+-e} "\n==> Security..."
-sudo $INSTPROG install -y gnupg pwgen
-sudo $INSTPROG install -y oathtool
+echo "Debian devel packages..."
+_install_packages exuberant-ctags
+_install_packages httpie
+_install_packages git tig
+_install_packages jq
+_install_packages perl libperl-dev
+# _install_packages ruby ruby-dev ruby-full
+
+echo "Debian devel libs..."
+_install_packages gettext
+_install_packages imagemagick
+_install_packages libsqlite3-0 libsqlite3-dev
+_install_packages libssl-dev
+_install_packages zlib1g zlib1g-dev
+
+echo "Debian security packages..."
+_install_packages gnupg pwgen
+_install_packages oathtool
 
 # #############################################################################
 # Cleanup
 
-echo ${BASH_VERSION:+-e} "\n\n==> Cleaning up APT repositories..."
-
+echo "Debian APT repository clean up..."
 sudo $APTPROG autoremove -y
 sudo $APTPROG clean -y
