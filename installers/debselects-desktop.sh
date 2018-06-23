7c3e83d2adb6d9d2f79da4af6dd62a24fbff5604
Cristian Stroparo <cstropz@gmail.com>
2018-06-23 10:50:11 -0300

    Refactor debselects-desktop.sh and formatted its output to a summarized way

--- installers/debselects-desktop.sh
+++ installers/debselects-desktop.sh
@@ -5,23 +5,41 @@
 # #############################################################################
 # Globals
 
-if [ -z "$INSTPROG" ] ; then
-  export INSTPROG=apt-get
-  which apt >/dev/null 2>&1 && export INSTPROG=apt
-fi
+# System installers
+export APTPROG=apt-get; which apt >/dev/null 2>&1 && export APTPROG=apt
+export INSTPROG="$APTPROG"
 
 # #############################################################################
-# Check OS
+# Helpers
+
+_install_packages () {
+  for package in "$@" ; do
+    echo "Installing '$package'..."
+    if ! sudo $INSTPROG install -y "$package" >/dev/null 2>/tmp/pkg-install-${package}.log ; then
+      echo "${PROGNAME:+$PROGNAME: }WARN: There was an error installing package '$package' - see '/tmp/pkg-install-${package}.log'." 1>&2
+    fi
+  done
+}
+
+_print_bar () {
+  echo "################################################################################"
+}
+
+_print_header () {
+  _print_bar
+  echo "$@"
+  _print_bar
+}
+
+# #############################################################################
+# Checks
 
 if ! egrep -i -q 'debian|ubuntu' /etc/*release ; then
   echo "FATAL: Only Debian/Ubuntu based distros are allowed to call this script ($0)" 1>&2
   exit 1
 fi
 
-# #############################################################################
-# Sudo check
-
-if ! which sudo ; then
+if ! which sudo >/dev/null 2>&1 ; then
   echo "FATAL: Please log in as root, install and then configure sudo for your user first.." 1>&2
   cat "Suggested commands:" <<EOF
 su -
@@ -32,38 +50,38 @@ EOF
 fi
 
 # #############################################################################
-# Update
-
-sudo $INSTPROG update
-
-echo ${BASH_VERSION:+-e} "\n==> Upgrade all packages? [y/N]\c" ; read answer
-[[ $answer = y ]] && sudo $INSTPROG upgrade -y
-
-# #############################################################################
 # Main
 
-echo ${BASH_VERSION:+-e} "\n==> Base desktop packages..."
-sudo $INSTPROG install -y bum ssh-askpass xbacklight xclip xscreensaver
-sudo $INSTPROG install -y gnome-themes-standard
-sudo $INSTPROG install -y gnome-themes-ubuntu gtk2-engines-xfce
-sudo $INSTPROG install -y ntfs-3g
+_print_header "Debian desktop package selects"
 
-echo ${BASH_VERSION:+-e} "\n==> Educational..."
-sudo $INSTPROG install -y gperiodic
+echo "Debian APT index update..."
+if ! sudo $APTPROG update >/dev/null 2>/tmp/apt-update-err.log ; then
+  echo "WARN: There was some failure during APT index update - see '/tmp/apt-update-err.log'." 1>&2
+fi
 
-echo ${BASH_VERSION:+-e} "\n==> Productivity..."
-sudo $INSTPROG install -y atril galculator guake meld
-sudo $INSTPROG install -y gnome-shell-pomodoro
-sudo $INSTPROG install -y shutter # screenshots
+echo ${BASH_VERSION:+-e} "Debian desktop - base packages..."
+_install_packages bum ssh-askpass xbacklight xclip xscreensaver
+_install_packages gnome-themes-standard
+_install_packages gnome-themes-ubuntu gtk2-engines-xfce
+_install_packages ntfs-3g
 
-echo ${BASH_VERSION:+-e} "\n==> Other packages..."
-sudo $INSTPROG install -y autorenamer
+echo ${BASH_VERSION:+-e} "Debian desktop - educational packages..."
+_install_packages gperiodic
+
+echo ${BASH_VERSION:+-e} "Debian desktop - productivity packages..."
+_install_packages atril galculator guake meld
+_install_packages gnome-shell-pomodoro
+_install_packages shutter # screenshots
+
+echo ${BASH_VERSION:+-e} "Debian desktop - miscellaneous packages..."
+_install_packages autorenamer
 
 # #############################################################################
 # Cleanup
 
-sudo $INSTPROG autoremove
-sudo $INSTPROG clean
+echo "Debian APT repository clean up..."
+sudo $APTPROG autoremove -y
+sudo $APTPROG clean -y
 
 # #############################################################################
 echo
