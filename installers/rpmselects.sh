2575fea9d629d0d131a77375ddac6a11ed7bae52
Cristian Stroparo <cstropz@gmail.com>
2018-06-23 10:39:19 -0300

    Refactor rpmselects.sh and reformat its output - wip (testing pending)

--- installers/rpmselects.sh
+++ installers/rpmselects.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Cristian Stroparo's dotfiles
-
 # Custom RPM package selection
 
 # #############################################################################
@@ -9,28 +8,57 @@
 
 PROGNAME=rpmselects.sh
 
-if [ -z "$RPMPROG" ] ; then
-  export RPMPROG=yum
-  which dnf >/dev/null 2>&1 && export RPMPROG=dnf
-fi
-
-if [ -z "$RPMGROUP" ] ; then
-  export RPMGROUP="yum groupinstall"
-  which dnf >/dev/null 2>&1 && export RPMGROUP="dnf group install"
-fi
+# System installers
+export RPMPROG=yum; which dnf >/dev/null 2>&1 && export RPMPROG=dnf
+export RPMGROUP="yum groupinstall"; which dnf >/dev/null 2>&1 && export RPMGROUP="dnf group install"
+export INSTPROG="$RPMPROG"
 
 # #############################################################################
-# Check OS
+# Helpers
 
-if ! egrep -i -q 'cent ?os|fedora|oracle|red ?hat' /etc/*release ; then
-  echo "FATAL: Only Red Hat based distros are allowed to call this script ($0)" 1>&2
+_is_el_family () { egrep -i -q '(cent ?os|oracle|red ?hat|fedora)' /etc/*release ; }
+_is_el () { egrep -i -q '(cent ?os|oracle|red ?hat)' /etc/*release ; }
+_is_el6 () { egrep -i -q '(cent ?os|oracle|red ?hat).* 6' /etc/*release ; }
+_is_el7 () { egrep -i -q '(cent ?os|oracle|red ?hat).* 7' /etc/*release ; }
+_is_fedora () { egrep -i -q 'fedora' /etc/*release ; }
+
+_install_packages () {
+  for package in "$@" ; do
+    echo "Installing '$package'..."
+    if ! sudo $INSTPROG install -y "$package" >/dev/null 2>/tmp/pkg-install-${package}.log ; then
+      echo "${PROGNAME:+$PROGNAME: }WARN: There was an error installing package '$package' - see '/tmp/pkg-install-${package}.log'." 1>&2
+    fi
+  done
+}
+
+_install_rpm_groups () {
+  for group in "$@" ; do
+    echo "Installing RPM group '$group'"
+    if ! sudo $RPMGROUP -y "$group" >/dev/null 2>/tmp/rpm-group-install-err-$group.log ; then
+      echo "${PROGNAME:+$PROGNAME: }WARN: There was an error with group '$group' - see '/tmp/rpm-group-install-err-$group.log'." 1>&2
+    fi
+  done
+}
+
+_print_bar () {
+  echo "################################################################################"
+}
+
+_print_header () {
+  _print_bar
+  echo "$@"
+  _print_bar
+}
+
+# #############################################################################
+# Checks
+
+if ! _is_el_family ; then
+  echo "FATAL: Only Enterprise Linux family allowed to call this script ($0)" 1>&2
   exit 1
 fi
 
-# #############################################################################
-# Sudo check
-
-if ! which sudo ; then
+if ! which sudo >/dev/null 2>&1 ; then
   echo "FATAL: Please log in as root, install and then configure sudo for your user first.." 1>&2
   cat "Suggested commands:" <<EOF
 su -
@@ -41,66 +69,58 @@ EOF
 fi
 
 # #############################################################################
-# EPEL (Extra Packages for Enterprise Linux)
-# https://fedoraproject.org/wiki/EPEL
+# Main
 
-echo ${BASH_VERSION:+-e} \
-  "\n\n==> EPEL (Extra Packages for Enterprise Linux)..."
+_print_header "Enterprise Linux package selects"
 
-if ! grep -i -q 'fedora' /etc/*release ; then
-  if egrep -i -q '(centos|oracle|red *hat).* 7' /etc/*release ; then
-    sudo $RPMPROG -y install \
-      https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# EPEL extra packages
+if ! _is_fedora ; then
+  echo "EPEL - Extra Packages for Enterprise Linux..." # https://fedoraproject.org/wiki/EPEL
+  if _is_el7 ; then
+    _install_packages https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
   else
-    sudo $RPMPROG install -q -y epel-release.noarch
+    _install_packages epel-release.noarch
   fi
 fi
 
-# #############################################################################
-# Main
-
+# New HOSTNAME setup, if any
 if [ -n "$NEWHOSTNAME" ] ; then
   echo "==> Setting up hostname to '${NEWHOSTNAME}'..." 1>&2
   sudo hostnamectl set-hostname "${NEWHOSTNAME:-andromeda}"
 fi
 
+# Package indexing setup
 sudo yum makecache fast
-# sudo $RPMPROG update -y
 
-echo ${BASH_VERSION:+-e} "\n\n==> Base packages..."
-sudo $RPMPROG install -q -y curl lftp rsync wget
-sudo $RPMPROG install -q -y --enablerepo=epel mosh
-sudo $RPMPROG install -q -y less
-sudo $RPMPROG install -q -y --enablerepo=epel p7zip p7zip-plugins lzip cabextract unrar
-which tmux >/dev/null 2>&1 || sudo $RPMPROG install -q -y tmux
-sudo $RPMPROG install -q -y sqlite libdbi-dbd-sqlite
-sudo $RPMPROG install -q -y --enablerepo=epel the_silver_searcher # ag
-sudo $RPMPROG install -q -y unzip zip
-sudo $RPMPROG install -q -y zsh
+echo "EL base packages..."
+_install_packages curl lftp rsync wget
+_install_packages --enablerepo=epel mosh
+_install_packages less
+_install_packages --enablerepo=epel p7zip p7zip-plugins lzip cabextract unrar
+which tmux >/dev/null 2>&1 || _install_packages tmux
+_install_packages sqlite libdbi-dbd-sqlite
+_install_packages --enablerepo=epel the_silver_searcher # ag
+_install_packages unzip zip
+_install_packages zsh
 
-echo ${BASH_VERSION:+-e} "\n\n==> Devel packages..."
+echo "EL devel packages..."
 
 sudo $RPMGROUP -q -y --enablerepo=epel 'Development Tools'
-sudo $RPMPROG install -q -y ctags
-# sudo $RPMPROG install -q -y golang
-sudo $RPMPROG install -q -y --enablerepo=epel jq
-sudo $RPMPROG install -q -y make
-sudo $RPMPROG install -q -y perl perl-devel perl-ExtUtils-Embed
-# sudo $RPMPROG install -q -y ruby ruby-devel
-sudo $RPMPROG install -q -y --enablerepo=epel tig # git
+_install_packages ctags
+# _install_packages golang
+_install_packages --enablerepo=epel jq
+_install_packages make
+_install_packages perl perl-devel perl-ExtUtils-Embed
+# _install_packages ruby ruby-devel
+_install_packages --enablerepo=epel tig # git
 
-echo ${BASH_VERSION:+-e} "\n==> Security..."
+echo "EL security packages..."
 sudo $INSTPROG install -q -y gnupg pwgen
 sudo $INSTPROG install -q -y oathtool oath-toolkit
 
-# #############################################################################
-# SELinux
+echo "EL security SELinux packages..."
+_install_packages setroubleshoot-server selinux-policy-devel
 
-echo ${BASH_VERSION:+-e} "\n\n==> SELinux..."
-sudo $RPMPROG install -q -y setroubleshoot-server selinux-policy-devel
-
-# #############################################################################
-# Specific to the distribution
-
-sudo $RPMPROG install -q -y yum-utils
+echo "EL system packages..."
+_install_packages yum-utils
 
