d0a908d3ac381e5d5464702b9ad82bd9cc43d0d9
Cristian Stroparo <cstropz@gmail.com>
2018-06-23 10:25:58 -0300

    Rename function _install_groups -> _install_rpm_groups

--- installers/setupxfce.sh
+++ installers/setupxfce.sh
@@ -39,20 +39,20 @@ _is_el6 () { egrep -i -q '(centos|oracle|red *hat).* 6' /etc/*release ; }
 _is_el7 () { egrep -i -q '(centos|oracle|red *hat).* 7' /etc/*release ; }
 _is_fedora () { egrep -i -q 'fedora' /etc/*release ; }
 
-_install_groups () {
-  for group in "$@" ; do
-    echo "Installing RPM group '$group'"
-    if ! sudo $RPMGROUP -y "$group" >/dev/null 2>/tmp/rpm-group-install-err-$group.log ; then
-      echo "${PROGNAME:+$PROGNAME: }WARN: There was an error with group '$group' - see '/tmp/rpm-group-install-err-$group.log'." 1>&2
-    fi
-  done
-}
-
 _install_packages () {
   for package in "$@" ; do
     echo "Installing '$package'..."
     if ! sudo $INSTPROG install -y "$package" >/dev/null 2>/tmp/pkg-install-${package}.log ; then
       echo "${PROGNAME:+$PROGNAME: }WARN: There was an error installing package '$package' - see '/tmp/pkg-install-${package}.log'." 1>&2
+    fi
+  done
+}
+
+_install_rpm_groups () {
+  for group in "$@" ; do
+    echo "Installing RPM group '$group'"
+    if ! sudo $RPMGROUP -y "$group" >/dev/null 2>/tmp/rpm-group-install-err-$group.log ; then
+      echo "${PROGNAME:+$PROGNAME: }WARN: There was an error with group '$group' - see '/tmp/rpm-group-install-err-$group.log'." 1>&2
     fi
   done
 }
@@ -80,15 +80,15 @@ _print_header "XFCE setup"
 
 # #############################################################################
 if _is_el_family ; then
-  _install_groups "x window system"
+  _install_rpm_groups "x window system"
   if ! _is_fedora ; then
     if _is_el6 ; then
-      _install_groups "desktop" "general purpose desktop"
+      _install_rpm_groups "desktop" "general purpose desktop"
     elif _is_el7 ; then
-      _install_groups "desktop" "server with gui" "mate desktop"
+      _install_rpm_groups "desktop" "server with gui" "mate desktop"
     fi
   fi
-  _install_groups xfce
+  _install_rpm_groups xfce
   _install_packages xorg-x11-fonts-Type1 xorg-x11-fonts-misc
 
   echo "XFCE Whisker Menu setup..."
