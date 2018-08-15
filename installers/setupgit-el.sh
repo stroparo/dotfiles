074521756b1a4db71702c0d16affe0bcf1e1e23d
Cristian Stroparo <cstropz@gmail.com>
2018-08-15 17:30:18 -0500

    Update setupgit-el

--- installers/setupgit-el.sh
+++ installers/setupgit-el.sh
@@ -7,8 +7,9 @@ echo "Setup Git for EL Enterprise Linux based distributions"
 # #############################################################################
 # Globals
 
-VER_FULL=2.15.0
-URL_GIT='https://www.kernel.org/pub/software/scm/git/git-${VER_FULL}.tar.gz'
+VER_FULL=2.18.0
+export GIT_PREFIX="/usr/local/git"
+export GIT_URL='https://www.kernel.org/pub/software/scm/git/git-${VER_FULL}.tar.gz'
 export RPMPROG=yum; which dnf >/dev/null 2>&1 && export RPMPROG=dnf
 
 # #############################################################################
@@ -32,17 +33,17 @@ fi
 $RPMPROG install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel \
   && $RPMPROG install -y gcc perl-ExtUtils-MakeMaker \
   && cd /usr/src \
-  && curl -o git-${VER_FULL}.tar.gz "$URL_GIT" \
+  && curl -o git-${VER_FULL}.tar.gz "$GIT_URL" \
   && tar xvzf git-${VER_FULL}.tar.gz \
   && cd git-${VER_FULL} \
-  && make prefix=/usr/local/git all \
-  && make prefix=/usr/local/git install
+  && make prefix=${GIT_PREFIX:-/usr/local/git} all doc info \
+  && make prefix=${GIT_PREFIX:-/usr/local/git} install install-doc install-html install-info
 
 # TODO: add update-alternativas logic as well as manpages slaves
 
 if ! grep -q '/git' /etc/bashrc ; then
-  echo "export PATH=/usr/local/git/bin:$PATH" >> /etc/bashrc
-  export PATH=/usr/local/git/bin:$PATH
+  echo "export PATH=${GIT_PREFIX:-/usr/local/git}/bin:$PATH" >> /etc/bashrc
+  export PATH=${GIT_PREFIX:-/usr/local/git}/bin:$PATH
 fi
 
 git --version
