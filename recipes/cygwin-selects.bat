34a0b752b492368635c169cf394caf75bf7172c1
Cristian Stroparo <cstropz@gmail.com>
2018-09-13 11:39:34 -0500

    Format some windows batch commands to lowercase

--- recipes/cygwin-selects.bat
+++ recipes/cygwin-selects.bat
@@ -1,38 +1,40 @@
-@ECHO OFF
-ECHO Cygwin installation automated
+@echo OFF
+echo Cygwin installation automated
 CD %~dp0
 
 SETLOCAL
-SET CYGCATS=archive,base,utils
-SET CYGSETUP=%CD%\setup-x86_64.exe
-SET SITE=http://mirrors.kernel.org/sourceware/cygwin/
-rem SET SITE=http://linorg.usp.br/cygwin/
-rem SET SITE=http://cygwin.osuosl.org/
-SET LOCALDIR=%LOCALAPPDATA%/cygwin
-SET ROOTDIR=C:/cygwin64
+set CYGCATS=archive,base,utils
+set CYGSETUP=%CD%\setup-x86_64.exe
+set SITE=http://mirrors.kernel.org/sourceware/cygwin/
+rem set SITE=http://linorg.usp.br/cygwin/
+rem set SITE=http://cygwin.osuosl.org/
+set LOCALDIR=%LOCALAPPDATA%/cygwin
+set ROOTDIR=C:/cygwin64
 
 if exist %CYGSETUP% goto OKSETUP
 powershell -Command "Import-Module BitsTransfer; Start-BitsTransfer http://cygwin.com/setup-x86_64.exe %CD%\setup-x86_64.exe"
 if exist %CYGSETUP% goto OKSETUP
 echo "Aborted as there is no setup-x86_64.exe neither could it be downloaded."
-PAUSE
+pause
 exit 1
 :OKSETUP
 
-ECHO INSTALLING PACKAGES..
+echo Cygwin installation started..
 
 rem %CYGSETUP% -q -v -a x86_64 -d -g -o -Y -O -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -C %CYGCATS%
-rem ECHO Categories just installed: %CYGCATS%
-rem PAUSE
+rem echo Categories just installed: %CYGCATS%
+rem pause
 
 %CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P bashdb,bash-completion,checkbashisms -P perl,python,python3,zsh -P p7zip -P unzip,zip
 %CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P curl,ping,rsync -P lftp -P wget,whois,wput
 %CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P mosh -P the_silver_searcher -P tmux
 %CYGSETUP% -q -v -a x86_64 -l "%LOCALDIR%" -R "%ROOTDIR%" -P git -P git-completion
 
-ECHO.
-ECHO Cygwin installation complete
+echo.
+echo Cygwin installation complete
+
 ENDLOCAL
-PAUSE
-EXIT /B 0
+
+pause
+exit /B 0
 
