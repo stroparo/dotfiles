bf197c86e9accdaa34c4c6bcc0b96cfc5933b0ba
Cristian Stroparo <cstropz@gmail.com>
2018-09-13 13:00:07 -0500

    Fix babunselects message

--- installers/babunselects.sh
+++ installers/babunselects.sh
@@ -8,15 +8,14 @@ CYGWINSETUP=cygwinselects.bat
 if ls "./installers/$CYGWINSETUP" >/dev/null 2>&1 ; then
   CYGWINSETUP="./installers/$CYGWINSETUP"
 elif [ ! -e "$CYGWINSETUP" ] ; then
-  echo "FATAL: Run in the same dir containing '$CYGWINSETUP'." 1>&2
+  echo "${PROGNAME:+PROGNAME: }FATAL: Run in the same dir containing '$CYGWINSETUP'." 1>&2
   exit 1
 fi
-echo "+PROGNAME: FATAL: There was some error." 1>&2
 
 # Requirements
 
 if ! type pact >/dev/null 2>&1 ; then
-  echo "+PROGNAME: FATAL: Not in Babun as the pact package installer program is not available." 1>&2
+  echo "${PROGNAME:+PROGNAME: }FATAL: Not in Babun as the pact package installer program is not available.." 1>&2
   exit 1
 fi
 
