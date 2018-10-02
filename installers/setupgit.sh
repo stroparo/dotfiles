152a756a3f8f5cff2e12ec23dbf1d49eb6cc7304
Cristian Stroparo <stroparo@outlook.com>
2018-10-02 10:22:59 -0300

    Rename setupgit-el to setupgit, deb distros still to be implemented

--- installers/setupgit.sh
+++ installers/setupgit.sh
@@ -2,7 +2,7 @@
 
 echo
 echo "################################################################################"
-echo "Setup Git for EL Enterprise Linux based distributions"
+echo "Setup Git from source"
 
 # #############################################################################
 # Globals
@@ -10,26 +10,26 @@ echo "Setup Git for EL Enterprise Linux based distributions"
 VER_FULL=2.18.0
 export GIT_PREFIX="/usr/local/git"
 export GIT_URL='https://www.kernel.org/pub/software/scm/git/git-${VER_FULL}.tar.gz'
+export PROFILE_STRING="export PATH=\"${GIT_PREFIX:-/usr/local/git}/bin:\$PATH\""
+
+# Package installers:
+export APTPROG=apt-get; which apt >/dev/null 2>&1 && export APTPROG=apt
 export RPMPROG=yum; which dnf >/dev/null 2>&1 && export RPMPROG=dnf
 
 # #############################################################################
 # Check OS
 
-if ! egrep -i -q 'cent ?os|fedora|oracle|red ?hat' /etc/*release ; then
-  echo "SKIP: Only Red Hat based distros are allowed to call this script ($0)" 1>&2
+if ! (uname -a | grep -i -q linux) ; then
+  echo "SKIP: Only Linux is allowed to call this script ($0)" 1>&2
   exit
 fi
 
 # #############################################################################
-# Pre-requisites
+# Main
 
-if $UID != 0 ; then
-  echo "SKIP: Must run this as root" 1>&2
-  exit
-fi
-
-# #############################################################################
-
+if egrep -i -q 'centos|fedora|oracle|red *hat' /etc/*release ; then
+  sudo $RPMPROG remove -y git
+  cat <<EOF | sudo bash
 $RPMPROG install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel \
   && $RPMPROG install -y gcc perl-ExtUtils-MakeMaker \
   && cd /usr/src \
@@ -38,18 +38,27 @@ $RPMPROG install -y curl-devel expat-devel gettext-devel openssl-devel zlib-deve
   && cd git-${VER_FULL} \
   && make prefix=${GIT_PREFIX:-/usr/local/git} all doc info \
   && make prefix=${GIT_PREFIX:-/usr/local/git} install install-doc install-html install-info
-
-# TODO: add update-alternativas logic as well as manpages slaves
-
-if ! grep -q '/git' /etc/bashrc ; then
-  echo "export PATH=${GIT_PREFIX:-/usr/local/git}/bin:$PATH" >> /etc/bashrc
-  export PATH=${GIT_PREFIX:-/usr/local/git}/bin:$PATH
+EOF
+  # TODO add update-alternativas logic as well as manpages slaves
+else egrep -i -q 'debian|ubuntu' /etc/*release ; then
+  : # TODO implement
 fi
 
+# #############################################################################
+# Post installation
+
+if [ ! -f /etc/profile.d/gitsrc.sh ] ; then
+  echo "${PROFILE_STRING}" > /etc/profile.d/gitsrc.sh
+  chmod 755 /etc/profile.d/gitsrc.sh
+fi
+
+echo "${PROGNAME:+$PROGNAME: }INFO: Git path:" 1>&2
+eval ${PROFILE_STRING}
+which -a git
 git --version
 
 # #############################################################################
 # Finish
 
-echo "FINISHED Git for EL setup"
+echo "FINISHED Git from source setup"
 echo
