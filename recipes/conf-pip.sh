02782beecde61d879cda5cd4de454422caed59c8
Cristian Stroparo <stroparo@outlook.com>
2018-11-30 13:29:34 -0200

    Output conf-pip header

--- recipes/conf-pip.sh
+++ recipes/conf-pip.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo
+echo "################################################################################"
+echo "Configure pip; \$0='$0'; \$PWD='$PWD'"
+
 mkdir -p "$HOME/.config/pip" 2>/dev/null
 cp -v "$HOME/.config/pip/pip.conf" "$HOME/.config/pip/pip.conf.$(date '+%Y%m%d-%OH%OM%OS')"
 cat > "$HOME/.config/pip/pip.conf" <<EOF
