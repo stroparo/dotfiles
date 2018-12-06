91e920b93278283832ab364e4d0c53b30a75e860
Cristian Stroparo <stroparo@outlook.com>
2018-12-06 08:38:31 -0600

    Format better conf-pip.sh

--- recipes/conf-pip.sh
+++ recipes/conf-pip.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
+PROGNAME="conf-pip.sh"
+
 echo
 echo "################################################################################"
 echo "Configure pip; \$0='$0'; \$PWD='$PWD'"
 
 mkdir -p "$HOME/.config/pip" 2>/dev/null
-cp -v "$HOME/.config/pip/pip.conf" "$HOME/.config/pip/pip.conf.$(date '+%Y%m%d-%OH%OM%OS')"
-cat > "$HOME/.config/pip/pip.conf" <<EOF
+
+if [ -e "$HOME/.config/pip/pip.conf" ] \
+  && ! cp -v "$HOME/.config/pip/pip.conf" "$HOME/.config/pip/pip.conf.$(date '+%Y%m%d-%OH%OM%OS')"
+then
+  echo "${PROGNAME:+$PROGNAME: }FATAL: Could not backup '$HOME/.config/pip/pip.conf'." 1>&2
+  exit 1
+fi
+
+touch "$HOME/.config/pip/pip.conf"
+if ! grep -q "format=columns" "$HOME/.config/pip/pip.conf" ; then
+  if [ -s "$HOME/.config/pip/pip.conf" ] ; then
+    echo >> "$HOME/.config/pip/pip.conf"
+  fi
+  cat >> "$HOME/.config/pip/pip.conf" <<EOF
 [list]
 format=columns
 EOF
+fi
