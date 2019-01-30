c81a3727e25e88d7fe8fb2bfde2cddd55f9bbca6
Cristian Stroparo <stroparo@outlook.com>
2019-01-30 15:24:34 -0600

    Update ui-term-colors assets

--- assets/ui-term-colors/cs-terminator.sh
+++
@@ -1,9 +0,0 @@
-# Color scheme configuration for Terminator terminal emulator (http://www.tenshu.net/terminator/ and https://launchpad.net/terminator)
-# 
-# Copy the following lines within the [profiles] section of terminator configuration file at ~/.config/terminator/config
-
-[[4bit-1531705693540]]
-  use_theme_colors = False
-  background_color = "#000000"
-  foreground_color = "#d9e6f2"
-  palette = "#000000:#c66c81:#81c66c:#c6b16c:#6c81c6:#b16cc6:#6cc6b1:#d9d9d9:#353535:#e3b5c0:#c0e3b5:#e3d8b5:#b5c0e3:#d8b5e3:#b5e3d8:#ffffff"
