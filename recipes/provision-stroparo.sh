a3a362dddf6cbfe996c3d9f3461837f9eaccf6a6
Cristian Stroparo <stroparo@outlook.com>
2019-06-12 12:09:43 -0300

    Fix sudo config command

--- recipes/provision-stroparo.sh
+++ recipes/provision-stroparo.sh
@@ -5,7 +5,7 @@ PROGNAME="provision-stroparo.sh"
 
 _step_base_system () {
   ${STEP_BASE_SYSTEM_DONE:-false} && return
-  [ -e /etc/sudoers ] && ! sudo grep -q "$USER" /etc/sudoers && (echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers)
+  [ -e /etc/sudoers ] && ! sudo grep -q "$USER" /etc/sudoers && (echo "$USER ALL=(ALL) NOPASSWD: ALL" | su -c 'tee -a /etc/sudoers' -)
   mkdir ~/workspace >/dev/null 2>&1 ; ls -d -l ~/workspace || exit $?
   bash -c "$(curl -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" || curl -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" entry.sh apps shell
   source "${DS_HOME:-$HOME/.ds}/ds.sh" || exit $?
