2260741b29c7740d434ff1cea2e3634ba6aa4691
Cristian Stroparo <cstropz@gmail.com>
2019-06-25 10:43:18 -0300

    Fix disable-ipv6 by putting in missing sudo calls

--- recipes/disable-ipv6.sh
+++ recipes/disable-ipv6.sh
@@ -11,7 +11,7 @@ if !(uname -a | grep -i -q linux) ; then
   exit
 fi
 
-if sudo grep -r -q "GRUB_CMDLINE_LINUX_DEFAULT.*ipv6.disable=1" /etc/default/grub /etc/default/grub.d ; then
+if sudo grep -r -q "GRUB_CMDLINE_LINUX_DEFAULT.*ipv6.disable=1" /etc/default/grub /etc/default/grub.d 2>/dev/null ; then
   echo "${PROGNAME:+$PROGNAME: }SKIP: IPv6 already disabled via grub default file." 1>&2
   exit
 fi
@@ -24,18 +24,21 @@ if [ -f /etc/default/grub.d/50_linuxmint.cfg ] ; then
   GRUB_FILE=/etc/default/grub.d/50_linuxmint.cfg
 fi
 unset GRUB_CMDLINE_FILE
-GRUB_CMDLINE_FILE="$(grep -l -r "GRUB_CMDLINE_LINUX_DEFAULT=" /etc/default/grub /etc/default/grub.d)"
+GRUB_CMDLINE_FILE="$(grep -l -r "GRUB_CMDLINE_LINUX_DEFAULT=" /etc/default/grub /etc/default/grub.d 2>/dev/null)"
 GRUB_FILE="${GRUB_CMDLINE_FILE:-${GRUB_FILE}}"
 
 # #############################################################################
 # Main
 
 if ! grep -q "GRUB_CMDLINE_LINUX_DEFAULT=" "${GRUB_FILE}" ; then
-  cat >> "${GRUB_FILE}" <<EOF
+  echo "${PROGNAME:+$PROGNAME: }INFO: Updating grub file '${GRUB_FILE}' with:"
+  sudo tee -a "${GRUB_FILE}" <<EOF
 GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
 EOF
 fi
-sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 /' "${GRUB_FILE}"
+
+echo "${PROGNAME:+$PROGNAME: }INFO: Updating grub file '${GRUB_FILE}' default Linux command with 'ipv6.disable=1'"
+sudo sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 /' "${GRUB_FILE}"
 
 sudo update-grub
 
