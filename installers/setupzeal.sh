dd34c10b86547dda4642758a1ed7125f4344feb0
Cristian Stroparo <stroparo@outlook.com>
2019-06-27 15:13:29 -0300

    Add zeal build from source capability in setupzeal

--- installers/setupzeal.sh
+++ installers/setupzeal.sh
@@ -1,22 +1,68 @@
 #!/usr/bin/env bash
 
-PROGNAME=setupzeal.sh
-export APTPROG=apt-get; which apt >/dev/null 2>&1 && export APTPROG=apt
+set -e
 
-echo "################################################################################"
-echo "Ubuntu PPA - Zeal offline docs application"
-echo "################################################################################"
+PROGNAME="setupzeal.sh"
 
-# #############################################################################
-# Early checks
-
-if ! egrep -i -q -r 'ubuntu' /etc/*release ; then
-  echo "${PROGNAME:+$PROGNAME: }SKIP: This is not an Ubuntu distribution." 1>&2
+if !(uname -a | grep -i -q linux) ; then
+  echo "SKIP: Only Linux is supported." 1>&2
   exit
 fi
 
+echo
+echo "################################################################################"
+echo "Zeal dev docs manager setup..."
+
 # #############################################################################
-# Helpers
+# Globals
+
+WORKDIR="${HOME}"
+
+export APTPROG=apt-get; which apt >/dev/null 2>&1 && export APTPROG=apt
+export RPMPROG=yum; which dnf >/dev/null 2>&1 && export RPMPROG=dnf
+
+ZEAL_URL="https://github.com/zealdocs/zeal/archive/master.zip"
+
+# #############################################################################
+# Build functions
+
+
+_install_system_deps () {
+  echo ${BASH_VERSION:+-e} "\n==> Installing dependencies..."
+  if egrep -i -q -r 'centos|fedora|oracle|red *hat' /etc/*release ; then
+    # sudo $RPMPROG install -y TODO
+    echo "${PROGNAME:+$PROGNAME: }SKIP: Enterprise Linux distributions still not supported." 1>&2
+    exit
+  elif egrep -i -q -r 'debian|ubuntu' /etc/*release ; then
+    sudo $APTPROG install -y cmake extra-cmake-modules libqt5webkit5-dev libqt5x11extras5-dev libarchive-dev libsqlite3-dev libxcb-keysyms1-dev
+  fi
+}
+
+
+_build_main () {
+  if which zeal >/dev/null 2>&1 && [ -d "${WORKDIR}"/."zeal" ] ; then
+    echo "${PROGNAME:+$PROGNAME: }SKIP: zeal likely already built and installed." 1>&2
+    return
+  fi
+
+  _install_system_deps
+
+  cd "${WORKDIR}"
+  if [ ! -e "${WORKDIR}/.zeal.zip" ] ; then
+    curl -LSfs -o "${WORKDIR}/.zeal.zip" "${ZEAL_URL}"
+  fi
+  unzip "${WORKDIR}/.zeal.zip" -d "${WORKDIR}"
+  mv "${WORKDIR}/zeal-master" "${WORKDIR}/.zeal"
+  cd "${WORKDIR}/.zeal"
+  mkdir build && cd build
+  cmake ..
+  make
+  sudo make install
+}
+
+
+# #############################################################################
+# Ubuntu Helpers
 
 _add_ppa_repo () {
 
@@ -30,7 +76,7 @@ _add_ppa_repo () {
   fi
 }
 
-_install_packages () {
+_install_apt_packages () {
   for package in "$@" ; do
     echo "Installing '$package'..."
     if ! sudo ${APTPROG} install -y "$package" >/tmp/pkg-install-${package}.log 2>&1 ; then
@@ -42,6 +88,16 @@ _install_packages () {
 # #############################################################################
 # Main
 
-dpkg -s zeal || _add_ppa_repo 'zeal-developers/ppa'
-sudo ${APTPROG} update
-_install_packages zeal
+if egrep -i -q -r 'ubuntu' /etc/*release ; then
+  if ! dpkg -s zeal ; then
+    _add_ppa_repo 'zeal-developers/ppa'
+    sudo ${APTPROG} update
+    _install_apt_packages zeal
+  fi
+else
+  echo "${PROGNAME:+$PROGNAME: }INFO: Building from source..." 1>&2
+  _build_main
+fi
+
+echo "////////////////////////////////////////////////////////////////////////////////"
+echo
