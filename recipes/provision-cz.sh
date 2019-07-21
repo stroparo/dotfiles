66eb159ca06998717552593bc211a579d7b03930
Cristian Stroparo <cstropz@gmail.com>
2019-07-21 19:41:06 -0300

    Update provision-cz.sh
    If crypto dir ($MOUNTS_PREFIX/z) is already active then add handy repo cloning into the z drive and gitcred setup.

--- recipes/provision-cz.sh
+++ recipes/provision-cz.sh
@@ -27,20 +27,45 @@ _step_base_system () {
 }
 
 
-_step_custom () {
+_step_ds_custom () {
   dsplugin.sh "stroparo@bitbucket.org/stroparo/ds-cz"
   if [ ! -f "${DS_HOME}/envcz.sh" ] ; then
     echo "${PROGNAME}: FATAL: no 'envcz.sh' found in DS_HOME (${DS_HOME})."
     exit 1
   fi
+}
 
+
+_step_ds_stroparo () {
   export PROVISION_OPTIONS="${PROVISION_OPTIONS} gui xfce"
   export RUNR_QUIET=true
   runr -c provision-stroparo
+}
 
+
+_step_expedite_pre_existing () {
+  if [ -d "${MOUNTS_PREFIX}/z" ] ; then
+    git clone "https://stroparo@bitbucket.org/stroparo/handy.git" "${MOUNTS_PREFIX}/z/handy"
+    
+    if [ -f "${MOUNTS_PREFIX}/z/gitcred.txt" ] ; then
+      git config --global credential.helper "store --file=${MOUNTS_PREFIX}/z/gitcred.txt"
+    fi
+  fi
+}
+
+
+_step_custom () {
+
+  _step_ds_custom
+  _step_ds_stroparo
+  source "${DS_HOME:-$HOME/.ds}/ds.sh" || exit $?
+  
+  _step_expedite_pre_existing
   bash "${DS_HOME:-$HOME/.ds}"/recipes/cz-conf-git.sh
   bash "${DS_HOME:-$HOME/.ds}"/scripts/czsetupautostart.sh
-  echo; echo "==> czsetup.sh <=="
+
+  echo
+  echo "==> czsetup.sh <=="
   echo
   echo "Review script \$DS_HOME/.../cz*filesystem*.sh" 1>&2
   echo "... and only then run czsetup.sh." 1>&2
