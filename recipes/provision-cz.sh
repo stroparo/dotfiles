0069c683ae29356d220f0b9bf5c50cd87200716f
Cristian Stroparo <stroparo@outlook.com>
2019-08-14 01:19:41 -0300

    Simplify some provisioning steps.

--- recipes/provision-cz.sh
+++ recipes/provision-cz.sh
@@ -4,6 +4,7 @@ PROGNAME="provision-cz.sh"
 
 
 _step_base_system () {
+
   ${STEP_BASE_SYSTEM_DONE:-false} && return
 
   if [ -e /etc/sudoers ] && ! sudo grep -q "${USER}.*ALL" /etc/sudoers ; then
@@ -27,8 +28,19 @@ _step_base_system () {
 }
 
 
-_step_ds_custom () {
+_step_expedite_pre_existing () {
+
+  if [ -d "${MOUNTS_PREFIX}/z" ] ; then
+    git clone "https://stroparo@bitbucket.org/stroparo/handy.git" "${MOUNTS_PREFIX}/z/handy"
+    git config --global credential.helper "store --file=${MOUNTS_PREFIX}/z/gitcred.txt"
+  fi
+}
+
+
+_step_ds_custom_plugins () {
+
   dsplugin.sh "stroparo@bitbucket.org/stroparo/ds-cz"
+
   if [ ! -f "${DS_HOME}/envcz.sh" ] ; then
     echo "${PROGNAME}: FATAL: no 'envcz.sh' found in DS_HOME (${DS_HOME})."
     exit 1
@@ -36,31 +48,21 @@ _step_ds_custom () {
 }
 
 
-_step_ds_stroparo () {
+_step_provision_custom () {
+
   export PROVISION_OPTIONS="${PROVISION_OPTIONS} gui xfce"
-  export RUNR_QUIET=true
   runr -c provision-stroparo
-}
-
-
-_step_expedite_pre_existing () {
-  if [ -d "${MOUNTS_PREFIX}/z" ] ; then
-    git clone "https://stroparo@bitbucket.org/stroparo/handy.git" "${MOUNTS_PREFIX}/z/handy"
-
-    if [ -f "${MOUNTS_PREFIX}/z/gitcred.txt" ] ; then
-      git config --global credential.helper "store --file=${MOUNTS_PREFIX}/z/gitcred.txt"
-    fi
-  fi
 }
 
 
 _step_custom () {
 
   _step_expedite_pre_existing
-  _step_ds_custom
-  _step_ds_stroparo
+  _step_ds_custom_plugins
+  _step_provision_custom
 
-  bash "${DS_HOME:-$HOME/.ds}"/recipes/cz-conf-git.sh
+  source "${DS_HOME:-$HOME/.ds}/ds.sh" || exit $?
+
   bash "${DS_HOME:-$HOME/.ds}"/scripts/czsetupautostart.sh
 
   echo
--- recipes/provision-stroparo.sh
+++ recipes/provision-stroparo.sh
@@ -4,38 +4,56 @@ PROGNAME="provision-stroparo.sh"
 
 
 _step_base_system () {
+
   ${STEP_BASE_SYSTEM_DONE:-false} && return
-  [ -e /etc/sudoers ] && ! sudo grep -q "$USER" /etc/sudoers && (echo "$USER ALL=(ALL) NOPASSWD: ALL" | su -c 'tee -a /etc/sudoers' -)
+
+  if [ -e /etc/sudoers ] && ! sudo grep -q "${USER}.*ALL" /etc/sudoers ; then
+    echo
+    echo "Add this line to your sudoers file:"
+    echo "$USER ALL=(ALL) NOPASSWD: ALL"
+    echo "Press any key to continue..."
+    read dummy
+    su - -c visudo
+  fi
+
   mkdir ~/workspace >/dev/null 2>&1 ; ls -d -l ~/workspace || exit $?
+
   bash -c "$(curl ${DLOPTEXTRA} -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
     || curl ${DLOPTEXTRA} -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
     entry.sh apps shell
+
   source "${DS_HOME:-$HOME/.ds}/ds.sh" || exit $?
+
   export STEP_BASE_SYSTEM_DONE=true
 }
 
 
-_step_setup_ds () {
+_step_ds_custom_plugins () {
+
   "${DS_HOME:-$HOME/.ds}/scripts/dsplugin.sh" "stroparo@bitbucket.org/stroparo/ds-stroparo" \
     || "${DS_HOME:-$HOME/.ds}/scripts/dsplugin.sh" "stroparo@github.com/stroparo/ds-stroparo"
-  dsload || . "${DS_HOME:-$HOME/.ds}/ds.sh" || exit $?
+
+  source "${DS_HOME:-$HOME/.ds}/ds.sh" || exit $?
 }
 
 
-_step_setup () {
+_step_custom () {
+
+  _step_ds_custom_plugins
+
   runr -c dotfiles
   runr -c git
   runr -c provision
 
   selects-python-stroparo.sh
-  st-conf-git.sh
+
+  bash "${DS_HOME:-$HOME/.ds}"/scripts/dsconfgit.sh
 }
 
 
 _main () {
   _step_base_system
-  _step_setup_ds
-  _step_setup
+  _step_custom
 }
 
 
