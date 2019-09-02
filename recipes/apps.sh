2311b000569d90c16f3e16ba192045adfdd04f22
Cristian Stroparo <stroparo@outlook.com>
2019-09-01 23:33:22 -0300

    Format recipe output according to the new standard.

--- recipes/apps.sh
+++ recipes/apps.sh
@@ -2,7 +2,7 @@
 
 PROGNAME="apps.sh"
 
-echo "$PROGNAME: INFO: started >>>> this is a compound recipe"
+echo "$PROGNAME: INFO: (no GUI) apps selections installation started >>>> this is a compound recipe"
 echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 
 # #############################################################################
@@ -12,5 +12,5 @@ cd "${RUNR_DIR:-$PWD}"
 bash "${RUNR_DIR:-$PWD}"/recipes/apps-debian.sh
 bash "${RUNR_DIR:-$PWD}"/recipes/apps-el.sh
 
-echo "$PROGNAME: COMPLETE: apps recipe (compound)"
+echo "$PROGNAME: COMPLETE: (no GUI) apps selections installation recipe (compound)"
 exit
--- recipes/shell.sh
+++ recipes/shell.sh
@@ -2,11 +2,18 @@
 
 PROGNAME="shell.sh"
 
-echo "$PROGNAME: INFO: started >>>> this is a compound recipe"
+echo "$PROGNAME: INFO: shell custom deployment started >>>> this is a compound recipe"
 echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 
-"${RUNR_DIR:-$PWD}"/installers/setupds.sh
-"${RUNR_DIR:-$PWD}"/installers/setupohmyzsh.sh
-"${RUNR_DIR:-$PWD}"/recipes/alias.sh
-"${RUNR_DIR:-$PWD}"/recipes/sshkeygen.sh
-"${RUNR_DIR:-$PWD}"/recipes/sshmodes.sh
+# #############################################################################
+
+cd "${RUNR_DIR:-$PWD}"
+
+bash "${RUNR_DIR:-$PWD}"/installers/setupds.sh
+bash "${RUNR_DIR:-$PWD}"/installers/setupohmyzsh.sh
+bash "${RUNR_DIR:-$PWD}"/recipes/alias.sh
+bash "${RUNR_DIR:-$PWD}"/recipes/sshkeygen.sh
+bash "${RUNR_DIR:-$PWD}"/recipes/sshmodes.sh
+
+echo "$PROGNAME: COMPLETE: shell custom deployment (compound)"
+exit
