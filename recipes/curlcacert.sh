2f6df755b6ce8befaf4f6da7e648fdc76aecf101
Cristian Stroparo <stroparo@outlook.com>
2019-09-01 23:10:19 -0300

    Format recipe output according to the new standard.

--- recipes/curlcacert.sh
+++ recipes/curlcacert.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+PROGNAME="curlcacert.sh"
+
+echo "$PROGNAME: INFO: started"
+echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
+
 cd
 curl -L -o "$HOME/.curl-cacert.pem" 'http://curl.haxx.se/ca/cacert.pem'
 
@@ -8,3 +13,6 @@ if ! grep -q 'CURL_CA_BUNDLE=' ~/.profile ; then
 export CURL_CA_BUNDLE="$HOME/.curl-cacert.pem"
 EOF
 fi
+
+echo "$PROGNAME: COMPLETE"
+exit
