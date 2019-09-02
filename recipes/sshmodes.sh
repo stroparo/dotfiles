c7762b73b26412a93c1bc99675144fcddee699e6
Cristian Stroparo <stroparo@outlook.com>
2019-09-01 23:44:50 -0300

    Format recipe output according to the new standard.

--- recipes/sshmodes.sh
+++ recipes/sshmodes.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-PROGNAME=sshmodes.sh
+PROGNAME="sshmodes.sh"
 
 # Set appropriate modes/permissions to SSH related files
 
-# #############################################################################
-# Prep
+echo "$PROGNAME: INFO: SSH file modes/permissions"
+echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 
-echo "################################################################################"
-echo "SSH file modes/permissions; \$0='$0'; \$PWD='$PWD'"
+# #############################################################################
 
 mkdir ~/.ssh 2>/dev/null
 if [ ! -d ~/.ssh ] ; then
@@ -16,16 +15,10 @@ if [ ! -d ~/.ssh ] ; then
   exit 1
 fi
 
-# #############################################################################
-# Main
-
 touch ~/.ssh/authorized_keys
 chmod -f -v 600 ~/.ssh/authorized_keys ~/.ssh/id*
 chmod -f -v 700 ~/.ssh
 ls -l ~/.ssh/authorized_keys ~/.ssh/id*
 
-# #############################################################################
-# Final sequence
-
-echo "${PROGNAME:+$PROGNAME: }INFO: complete." 1>&2
-echo
+echo "$PROGNAME: COMPLETE"
+exit
