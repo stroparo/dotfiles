29efef993efcb5061972360a17465ee3e7f5e038
Cristian Stroparo <stroparo@outlook.com>
2019-09-01 22:41:36 -0300

    Format recipe output according to the new standard.

--- recipes/shell.sh
+++ recipes/shell.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+PROGNAME="shell.sh"
+
+echo "$PROGNAME: INFO: started >>>> this is a compound recipe"
+echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
+
 "${RUNR_DIR:-$PWD}"/installers/setupds.sh
 "${RUNR_DIR:-$PWD}"/installers/setupohmyzsh.sh
 "${RUNR_DIR:-$PWD}"/recipes/alias.sh
