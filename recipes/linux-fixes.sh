7aee63fda4582242164cffae7f6d6e49ea8a205e
Cristian Stroparo <stroparo@outlook.com>
2019-09-01 22:39:08 -0300

    Format recipe output according to the new standard.

--- recipes/linux-fixes.sh
+++ recipes/linux-fixes.sh
@@ -4,11 +4,16 @@ PROGNAME="linux-fixes.sh"
 
 if ! (uname | grep -i -q linux) ; then echo "$PROGNAME: SKIP: Linux supported only" ; exit ; fi
 
-echo "$PROGNAME: INFO: Applying Linux fixes in '${RUNR_DIR:-.}/recipes-linux-fixes'..."
+echo "$PROGNAME: INFO: started >>>> this is a compound recipe"
+echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
+
+# #############################################################################
+
+echo "$PROGNAME: INFO: executing '${RUNR_DIR:-.}/recipes-linux-fixes/fix-*'..."
 
 for fix in "${RUNR_DIR:-.}"/recipes-linux-fixes/fix-* ; do
   bash "${fix}"
 done
 
-echo "$PROGNAME: COMPLETE: Linux fixes in '${RUNR_DIR:-.}/recipes-linux-fixes'"
+echo "$PROGNAME: COMPLETE: '${RUNR_DIR:-.}/recipes-linux-fixes' (compound)"
 exit
