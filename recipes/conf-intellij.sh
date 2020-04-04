2ec3faa8beadb24313eb065e4b412ce2264ea452
Cristian Stroparo <stroparo@outlook.com>
2020-04-03 20:06:48 -0500

    Add conf-intellij recipe

---
+++ recipes/conf-intellij.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+PROGNAME="conf-intellij.sh"
+
+echo "$PROGNAME: INFO: Intellij custom config started"
+
+# #############################################################################
+# Globals
+
+SRC_CONFIG_DIR="${RUNR_DIR:-$PWD}/config/intellij"
+
+# #############################################################################
+# Prep User PATH
+
+if (uname -a | egrep -i -q "cygwin|mingw|msys|win32|windows") ; then
+
+  INTELLIJ_WIN="$(cygpath "${USERPROFILE}")/.IntelliJIdea"
+  /cygdrive/c/Users/e076373/.IntelliJIdea2019.1/config/options/vim_settings.xml
+
+elif [[ "$(uname -a)" = *[Ll]inux* ]] ; then
+  # TODO review
+  INTELLIJ_USER="${HOME}/.config/intellij"
+fi
+
+# #############################################################################
+# Deploy
+
+for iconfigdir in $(ls -1d "${INTELLIJ_WIN}"/*/) ; do
+  echo "cp -f -R \"${SRC_CONFIG_DIR}\"/* \"${iconfigdir}\"/ ..."
+  cp -f -R "${SRC_CONFIG_DIR}"/* "${iconfigdir}"/
+done
+
+echo "$PROGNAME: COMPLETE"
+exit
