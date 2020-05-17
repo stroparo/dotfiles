94c4b72049c6b5cdfd6fcbe017bce5c4dde7e4ed
Cristian Stroparo <stroparo@outlook.com>
2020-05-17 16:47:35 -0300

    Compile tmux only if not in Ubuntu

--- installers/setuptmux.sh
+++ installers/setuptmux.sh
@@ -18,14 +18,16 @@ WORKDIR="$HOME"
 export APTPROG=apt-get; which apt >/dev/null 2>&1 && export APTPROG=apt
 export RPMPROG=yum; which dnf >/dev/null 2>&1 && export RPMPROG=dnf
 
+TMUX_VERSION=3.1b
+TMUX_URL="https://github.com/tmux/tmux/archive/${TMUX_VERSION}.tar.gz"
+
+# 2.1.8 version of libevent was confirmed compatible with tmux 2.6
 LIBEVENT_VERSION=2.1.8
-LIBEVENT_URL="https://github.com/libevent/libevent/releases/download/release-$LIBEVENT_VERSION-stable/libevent-$LIBEVENT_VERSION-stable.tar.gz"
+LIBEVENT_URL="https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}-stable/libevent-${LIBEVENT_VERSION}-stable.tar.gz"
 
+# 6.1 version of ncurses was confirmed compatible with tmux 2.6
 NCURSES_VERSION=6.1
-NCURSES_URL="https://ftp.gnu.org/pub/gnu/ncurses/ncurses-$NCURSES_VERSION.tar.gz"
-
-TMUX_VERSION=2.6
-TMUX_URL="https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION.tar.gz"
+NCURSES_URL="https://ftp.gnu.org/pub/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz"
 
 # #############################################################################
 # Functions
@@ -50,6 +52,9 @@ _install_system_deps () {
     sudo $RPMPROG install -y ncurses ncurses-devel
   elif egrep -i -q -r 'debian|ubuntu' /etc/*release ; then
     sudo $APTPROG install -y automake make gcc
+    sudo $RPMPROG install -y build-essential bison pkg-config
+    sudo $RPMPROG install -y libevent libevent-dev libevent-headers
+    sudo $RPMPROG install -y ncurses ncurses-dev
   fi
 }
 
@@ -86,8 +91,8 @@ _build_ncurses () {
 
 _build_deps () {
   _install_system_deps
-  _build_libevent
-  _build_ncurses
+  # _build_libevent
+  # _build_ncurses
 }
 
 
@@ -95,23 +100,32 @@ _build_main () {
 
   cd "$WORKDIR"
 
-  if which tmux >/dev/null 2>&1 && [ -d "${PWD}/.tmux-$TMUX_VERSION" ] ; then
-    echo "${PROGNAME:+$PROGNAME: }SKIP: tmux likely already built (from '${PWD}/.tmux-$TMUX_VERSION') and installed." 1>&2
+  if which tmux >/dev/null 2>&1 && [ -d "${PWD}/.tmux-${TMUX_VERSION}" ] ; then
+    echo "${PROGNAME:+$PROGNAME: }SKIP: tmux likely already built (from '${PWD}/.tmux-${TMUX_VERSION}') and installed." 1>&2
     return
   fi
 
   _build_deps
 
-  if [ ! -e ./."tmux-$TMUX_VERSION".tar.gz ] ; then
-    curl ${DLOPTEXTRA} -LSfs -o ./."tmux-$TMUX_VERSION".tar.gz "$TMUX_URL"
+  if [ ! -e ./."tmux-${TMUX_VERSION}".tar.gz ] ; then
+    curl ${DLOPTEXTRA} -LSfs -o ./."tmux-${TMUX_VERSION}".tar.gz "$TMUX_URL"
   fi
-  tar -xzf ./."tmux-$TMUX_VERSION".tar.gz
-  mv "tmux-$TMUX_VERSION" ."tmux-$TMUX_VERSION"
-  cd ."tmux-$TMUX_VERSION"
+  tar -xzf ./."tmux-${TMUX_VERSION}".tar.gz
+  mv "tmux-${TMUX_VERSION}" ."tmux-${TMUX_VERSION}"
+  cd ."tmux-${TMUX_VERSION}"
   LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib" \
     ./configure --prefix="$PREFIX"
   make
   _make_install
+}
+
+
+_build_prep () {
+  mkdir -p "$PREFIX"/
+  if [ ! -d "$PREFIX" ] ; then
+    echo "FATAL: No prefix dir ($PREFIX)." 1>&2
+    exit 1
+  fi
 }
 
 
@@ -124,13 +138,15 @@ _install_plugin_manager () {
 # #############################################################################
 # Main
 
-mkdir -p "$PREFIX"/
-if [ ! -d "$PREFIX" ] ; then
-  echo "FATAL: No prefix dir ($PREFIX)." 1>&2
-  exit 1
+if egrep -i -q -r 'ubuntu' /etc/*release ; then
+  # Ubuntu 20.04 has 3.1a which is current as of the latest
+  # commit of this recipe, so just install the package:
+  sudo apt install -y tmux
+else
+  _build_prep
+  _build_main
 fi
 
-_build_main
 _install_plugin_manager
 
 echo "$PROGNAME: COMPLETE: tmux setup"
