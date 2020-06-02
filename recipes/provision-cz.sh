bb53d6f6f607bccd7f6a4b443bc4476d28601843
Cristian Stroparo <stroparo@outlook.com>
2020-06-01 21:21:18 -0300

    Add node recipe to provision*

--- recipes/provision-cz.sh
+++ recipes/provision-cz.sh
@@ -7,7 +7,7 @@ REPO_DS_CZ="stroparo@bitbucket.org/stroparo/ds-cz"
 REPO_DS_CZ_ALTN="stroparo@github.com/stroparo/ds-cz"
 
 # Recipes:
-export PROVISION_OPTIONS="base sudonopasswd gui chrome golang python rust"
+export PROVISION_OPTIONS="base sudonopasswd gui chrome golang node python rust"
 bash "${RUNR_DIR}"/recipes/provision.sh
 bash "${RUNR_DIR}"/recipes/keyb-ez.sh
 
