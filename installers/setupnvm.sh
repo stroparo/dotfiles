db53a07b611c634a0d5cb067570747f4a0ea6cf7
Cristian Stroparo <stroparo@outlook.com>
2020-06-01 21:37:57 -0300

    Add failsafe for shell profiling sources in setupnvm

--- installers/setupnvm.sh
+++ installers/setupnvm.sh
@@ -9,15 +9,23 @@ if command -v nvm ; then echo "$PROGNAME: SKIP: already installed." ; exit ; fi
 # Globals:
 if ${IGNORE_SSL:-false} ; then export DLOPTEXTRA="-k ${DLOPTEXTRA}" ; fi
 NVM_VERSION="v0.35.3"
+NVM_INSTALL_URL="https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh"
+NVM_INIT_CODE='
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+'
 
 
 echo "$PROGNAME: INFO: nvm (version: ${NVM_VERSION}) setup started"
 echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 
-if ! curl -o- ${DLOPTEXTRA} "https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh" ; then
+if ! bash -c "curl -o- ${DLOPTEXTRA} \"${NVM_INSTALL_URL}\"" ; then
   echo "${PROGNAME:+$PROGNAME: }FATAL: There was some error." 1>&2
   exit 1
 fi
+if ! grep -q "NVM_DIR=" ~/.bashrc ; then echo "${NVM_INIT_CODE}" >> ~/.bashrc ; fi
+if ! grep -q "NVM_DIR=" ~/.zshrc ; then echo "${NVM_INIT_CODE}" >> ~/.zshrc ; fi
 
 echo "$PROGNAME: COMPLETE: nvm setup"
 exit
