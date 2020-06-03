2d44cc1baf9f4bfc5653b2bb58677466039e01ee
Cristian Stroparo <stroparo@outlook.com>
2020-06-03 19:12:46 -0300

    Update editor conf recipes

--- recipes/conf-subl.sh
+++ recipes/conf-subl.sh
@@ -1,68 +1,38 @@
 #!/usr/bin/env bash
 
 PROGNAME="conf-subl.sh"
-
-echo "$PROGNAME: INFO: SublimeText custom config started"
-
-# #############################################################################
-# Globals
+_exit () { echo ; echo ; echo ; exit 0 ; }
+_exiterr () { echo "$2" 1>&2 ; echo 1>&2 ; echo 1>&2 ; exit "$1" ; }
 
 SRC_CONFIG_DIR="${RUNR_DIR:-$PWD}/config/subl"
+if [ -z "$SRC_CONFIG_DIR" ] ; then _exiterr 1 "${PROGNAME}: FATAL: No dir '${SRC_CONFIG_DIR}'." ; fi
 
-# #############################################################################
-# Prep User PATH
-
-if (uname -a | egrep -i -q "cygwin|mingw|msys|win32|windows") ; then
-
-  SUBL_WIN="$(cygpath "${USERPROFILE}")"'/AppData/Roaming/Sublime Text 3'
-
-  if [ -d "${SUBL_WIN}" ] ; then
-    SUBL_USER="${SUBL_WIN}/Packages/User"
-  fi
-
-  # Prefer an opt dir sublime_text instance instead, if any:
-  if [ -d "$(cygpath "${USERPROFILE}")/opt/subl" ] ; then
-    SUBL_USER="$(cygpath "${USERPROFILE}")/opt/subl/Data/Packages/User"
-  elif [ -d "/c/opt/subl" ] ; then
-    SUBL_USER="/c/opt/subl/Data/Packages/User"
-  elif [ -d "/cygdrive/c/opt/subl" ] ; then
-    SUBL_USER="/cygdrive/c/opt/subl/Data/Packages/User"
-  fi
-elif [[ "$(uname -a)" = *[Ll]inux* ]] ; then
-  SUBL_USER="${HOME}/.config/sublime-text-3/Packages/User"
-fi
-
-if [ ! -d "${SUBL_USER}" ] ; then
-  echo "${PROGNAME:+$PROGNAME: }SKIP configuration as there is no SUBL_USER dir ('${SUBL_USER}')."
-  exit
-fi
-
-# #############################################################################
-# Deploy
-
+# Global SUBL_USER:
+SUBL_USER="$HOME/.config/sublime-text-3/Packages/User"
+SUBL_WIN="$(cygpath "$USERPROFILE" 2>/dev/null)/AppData/Roaming/Sublime Text 3"
+SUBL_WIN_OPT="$(cygpath "$USERPROFILE" 2>/dev/null)/opt/subl"
+if [ -d "${SUBL_WIN}" ] ; then SUBL_USER="${SUBL_WIN}/Packages/User" ; fi
+if [ -d "${SUBL_WIN_OPT}" ] ; then SUBL_USER="${SUBL_WIN_OPT}/Data/Packages/User" ; fi
 mkdir -p "${SUBL_USER}"
+if [ ! -d "$SUBL_USER" ] ; then _exiterr 1 "${PROGNAME}: FATAL: Could not create SUBL_USER dir '$SUBL_USER'." ; fi
 
-if [ -z "$SRC_CONFIG_DIR" ] ; then
-  echo "${PROGNAME:+$PROGNAME: }FATAL: No source config dir found ($SRC_CONFIG_DIR)." 1>&2
-  exit 1
-fi
+
+echo "$PROGNAME: INFO: SublimeText custom config started..."
+
+
+# Copy config files:
 config_filenames="$(ls -1d "${SRC_CONFIG_DIR}"/*)"
 config_filenames="$(echo "$config_filenames" | sed "s/^/'/" | sed "s/$/'/" | tr '\n' ' ')" # prep for eval
-
 if ! eval cp -v -L -R "${config_filenames}" "\"${SUBL_USER}\""/ ; then
   echo "${PROGNAME:+$PROGNAME: }ERROR: deploying sublimetext files." 1>&2
 fi
 
-# #############################################################################
 # Package Control ignored plugins if not in Linux
-
 if ! (uname -a | grep -i -q linux) ; then
   sed -i -e 's#"Git#// "Git#' "${SUBL_USER}/Package Control.sublime-settings"
 fi
 
-# #############################################################################
-# Packages no longer maintained, installed from this repo's assets dir
-
+# Packages no longer online, installed from this repo's assets dir
 if [ -d "${RUNR_DIR:-$PWD}/assets/subl-packages" ] && [ -d "${SUBL_USER}/../../Installed Packages" ] ; then
   if ! cp -v -L -R "${RUNR_DIR:-$PWD}/assets/subl-packages"/* "${SUBL_USER}/../../Installed Packages"/ ; then
     echo "${PROGNAME:+$PROGNAME: }ERROR: deploying local 'Installed Packages'." 1>&2
@@ -72,16 +42,9 @@ if [ -d "${RUNR_DIR:-$PWD}/assets/subl-packages" ] && [ -d "${SUBL_USER}/../../I
   fi
 fi
 
-# #############################################################################
-# Symlink for any existing portable instance
-
-if (uname -a | grep -i -q linux) \
-  && [ ! -e /usr/local/bin/subl ] \
-  && which sublime_text >/dev/null 2>&1 \
-  && [[ $(which sublime_text) != ${HOME}* ]]
-then
+# Symlink for any existing instance not in HOME:
+if which sublime_text >/dev/null 2>&1 && [[ $(which sublime_text) != ${HOME}* ]] ; then
   sudo ln -s $(which sublime_text) /usr/local/bin/subl
 fi
 
-echo "$PROGNAME: COMPLETE"
-exit
+_exit "$PROGNAME: COMPLETE"
--- recipes/conf-vscodium.sh
+++ recipes/conf-vscodium.sh
@@ -1,71 +1,36 @@
 #!/usr/bin/env bash
 
 PROGNAME="conf-vscodium.sh"
-
-echo "$PROGNAME: INFO: Visual Studio Code custom config started"
-
-# #############################################################################
-# Globals
+_exit () { echo ; echo ; echo ; exit 0 ; }
+_exiterr () { echo "$2" 1>&2 ; echo 1>&2 ; echo 1>&2 ; exit "$1" ; }
 
 SRC_CONFIG_DIR="${RUNR_DIR:-$PWD}/config/vscodium"
+if [ -z "$SRC_CONFIG_DIR" ] ; then _exiterr 1 "${PROGNAME}: FATAL: No dir '${SRC_CONFIG_DIR}'." ; fi
+
 export VSCODECMD="codium"
+if ! which ${VSCODECMD} >/dev/null 2>&1 ; then _exit "${PROGNAME}: SKIP: ${VSCODECMD} not available." ; fi
 
-# #############################################################################
-# Requirements
+# Global CODE_USER_DIR:
+if which cygpath >/dev/null 2>&1 ; then CODE_USER_DIR="$(cygpath "${USERPROFILE}" 2>/dev/null)/AppData/Roaming/VSCodium/User" ; fi
+if [[ "$(uname -a)" = *[Ll]inux* ]] ; then CODE_USER_DIR="${HOME}/.config/VSCodium/User" ; fi
+mkdir -p "${CODE_USER_DIR}"
+if [ ! -d "${CODE_USER_DIR}" ] ; then _exit "${PROGNAME}: SKIP: no dir '$CODE_USER_DIR'." ; fi
 
-if ! which ${VSCODECMD} >/dev/null 2>&1 ; then
-  echo "${PROGNAME:+$PROGNAME: }SKIP: ${VSCODECMD} not available."
-  exit
-fi
 
-# #############################################################################
+echo "$PROGNAME: INFO: VSCodium custom config started..."
+
+
 # Set as default editor
-
-if which xdg-mime >/dev/null 2>&1 ; then
-  xdg-mime default vscodium.desktop text/plain
-fi
-
-if which update-alternatives >/dev/null 2>&1 \
-  && egrep -i -q -r 'debian|ubuntu' /etc/*release
-then
+if which xdg-mime >/dev/null 2>&1 ; then xdg-mime default vscodium.desktop text/plain ; fi
+if which update-alternatives >/dev/null 2>&1 && egrep -i -q -r 'debian|ubuntu' /etc/*release ; then
   sudo update-alternatives --set editor "$(which ${VSCODECMD})"
 fi
 
-# #############################################################################
-# Conf - User settings
-
-if (uname -a | egrep -i -q "cygwin|mingw|msys|win32|windows") ; then
-  CODE_USER_DIR="$(cygpath "${USERPROFILE}")/AppData/Roaming/VSCodium/User"
-elif [[ "$(uname -a)" = *[Ll]inux* ]] ; then
-  CODE_USER_DIR="${HOME}/.config/VSCodium/User"
-fi
-
-# Start vscodium for it to create the user dir, then kill it right away:
-${VSCODECMD} >/dev/null 2>&1 &
-sleep 8
-kill %1
-
-if [ ! -d "${CODE_USER_DIR}" ] ; then
-  echo "${PROGNAME:+$PROGNAME: }SKIP configuration as there is no CODE_USER_DIR dir ('$CODE_USER_DIR')."
-  exit
-fi
-
-if [ -z "$SRC_CONFIG_DIR" ] ; then
-  echo "${PROGNAME:+$PROGNAME: }FATAL: No source config dir found ($SRC_CONFIG_DIR)." 1>&2
-  exit 1
-fi
+# Copy config files:
 config_filenames="$(ls -1d ${SRC_CONFIG_DIR}/*)"
 config_filenames="$(echo "$config_filenames" | sed "s/^/'/" | sed "s/$/'/" | tr '\n' ' ')" # prep for eval
-
 if ! eval cp -L -R "${config_filenames}" "\"${CODE_USER_DIR}\""/ ; then
-  echo "${PROGNAME:+$PROGNAME: }ERROR deploying VSCode configuration files." 1>&2
+  _exiterr 1 "${PROGNAME}: FATAL: deploying VSCode configuration files."
 fi
 
-# #############################################################################
-# Final sequence
-
-echo "$PROGNAME: COMPLETE"
-echo
-echo
-
-exit
+_exit "$PROGNAME: COMPLETE"
