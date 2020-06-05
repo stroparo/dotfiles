5fe0ddd9b158f52b42fb47544f268ef3be109237
Cristian Stroparo <stroparo@outlook.com>
2020-06-05 17:14:49 -0300

    Format

--- recipes/vscodium.sh
+++ recipes/vscodium.sh
@@ -2,15 +2,16 @@
 
 PROGNAME="vscodium.sh"
 
-echo "$PROGNAME: INFO: Visual Studio Codium custom deployment >>>> this is a compound recipe"
+
+echo "$PROGNAME: INFO: started (compound)..."
 echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 
-# #############################################################################
 
 cd "${RUNR_DIR:-$PWD}"
 
 bash "${RUNR_DIR:-$PWD}"/installers/setupvscodium.sh
 bash "${RUNR_DIR:-$PWD}"/recipes/conf-vscodium.sh
 
-echo "$PROGNAME: COMPLETE: Visual Studio Codium custom deployment (compound)"
+
+echo "$PROGNAME: COMPLETE (compound)"
 exit
