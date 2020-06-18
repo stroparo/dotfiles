5d1b72eada7f366d6e7987841e763c662db6f36e
Cristian Stroparo <stroparo@outlook.com>
2020-06-18 19:30:07 -0300

    Add spaceship ohmyzsh theme configuration

--- installers/setupohmyzsh.sh
+++ installers/setupohmyzsh.sh
@@ -78,8 +78,35 @@ if [ ! -e "$ZSH_CUSTOM/themes/spaceship.zsh-theme" ] ; then
 fi
 
 
-
 # #############################################################################
+# Themes
+
+_conf_spaceship () {
+  if grep SPACESHIP_PROMPT_ORDER ~/.zshrc ; then
+    return
+  fi
+  cat >> ~/.zshrc <<'EOF'
+SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  exec_time     # Execution time
+  line_sep      # Line break
+  vi_mode       # Vi-mode indicator
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+EOF
+}
+
+
 _select_theme () {
   echo ; echo "Selecting ZSH_THEME=\"${1}\"" ; echo
   if ! grep 'ZSH_THEME=' "${HOME}/.zshrc" ; then
@@ -87,8 +114,10 @@ _select_theme () {
   fi
   sed -i -e "s/^ZSH_THEME=.*$/ZSH_THEME=\"${1}\"/" "${HOME}/.zshrc"
 }
+
+
 # _select_theme robbyrussell
-_select_theme spaceship
+_select_theme spaceship ; _conf_spaceship
 
 # #############################################################################
 # Final sequence
