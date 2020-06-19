9bf4fe3790e99a2a23d23d75760a3c44adc8e6e1
Cristian Stroparo <stroparo@outlook.com>
2020-06-19 19:16:39 -0300

    Add setupyarn to nodejs

--- recipes/nodejs.sh
+++ recipes/nodejs.sh
@@ -12,6 +12,7 @@ echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 cd "${RUNR_DIR:-$PWD}"
 
 bash "${RUNR_DIR:-$PWD}"/installers/setupnvm.sh
+bash "${RUNR_DIR:-$PWD}"/installers/setupyarn.sh
 
 # bash "${RUNR_DIR:-$PWD}"/recipes/...
 
