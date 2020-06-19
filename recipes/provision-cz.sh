b0d09813a45e447bb0a31a89861f2913a9cbb93f
Cristian Stroparo <stroparo@outlook.com>
2020-06-19 15:19:01 -0300

    Have devel platform provisioning only occur in provision-stroparo (cz relies on stroparo anyway)

--- recipes/provision-cz.sh
+++ recipes/provision-cz.sh
@@ -7,7 +7,7 @@ REPO_DS_CZ="stroparo@bitbucket.org/stroparo/ds-cz"
 REPO_DS_CZ_ALTN="stroparo@github.com/stroparo/ds-cz"
 
 # Recipes:
-export PROVISION_OPTIONS="base sudonopasswd gui chrome golang nodejs python rust"
+export PROVISION_OPTIONS="base sudonopasswd gui xfce chrome"
 bash "${RUNR_DIR}"/recipes/provision.sh
 bash "${RUNR_DIR}"/recipes/keyb-ez.sh
 
--- recipes/provision-stroparo.sh
+++ recipes/provision-stroparo.sh
@@ -5,12 +5,22 @@
 
 REPO_DS_ST="stroparo@bitbucket.org/stroparo/ds-stroparo"
 REPO_DS_ST_ALTN="stroparo@github.com/stroparo/ds-stroparo"
+PROVISION_DEVEL="golang nodejs python rust"
+
+
+if [ ${PROGNAME} != 'provision-stroparo.sh' ] ; then
+  echo ; echo ; echo "${PROGNAME:+$PROGNAME: }INFO: provision-stroparo.sh steps from this point on..." 1>&2
+  echo '...' ; echo ; echo
+fi
+
 
 # Recipes:
 if [ -z "${PROVISION_OPTIONS}" ] ; then
-  export PROVISION_OPTIONS="base gui chrome golang nodejs python rust"
-  bash "${RUNR_DIR}"/recipes/provision.sh
+  export PROVISION_OPTIONS="base gui xfce chrome ${PROVISION_DEVEL}"
+else
+  export PROVISION_OPTIONS="nodevel ${PROVISION_DEVEL}"
 fi
+bash "${RUNR_DIR}"/recipes/provision.sh
 bash "${RUNR_DIR}"/recipes/dotfiles.sh
 bash "${RUNR_DIR}"/recipes/git.sh
 
@@ -19,3 +29,10 @@ source "${RUNR_DIR:-.}"/helpers/dsenforce.sh
 if ! (dsplugin.sh "$REPO_DS_ST" || dsplugin.sh "$REPO_DS_ST_ALTN") ; then exit 1 ; fi
 bash "${DS_HOME:-$HOME/.ds}"/scripts/dsconfgit.sh
 bash "${DS_HOME:-$HOME/.ds}"/scripts/selects-python-stroparo.sh
+
+
+if [ ${PROGNAME} != 'provision-stroparo.sh' ] ; then
+  echo ; echo ; echo '...'
+  echo "${PROGNAME:+$PROGNAME: }INFO: provision-stroparo.sh steps COMPLETE" 1>&2
+  echo ; echo
+fi
