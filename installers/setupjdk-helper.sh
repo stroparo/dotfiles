4348cb3b140a310dca8b0314535716067b5c4410
Cristian Stroparo <stroparo@outlook.com>
2020-06-24 21:18:47 -0300

    Update jdk recipes, rename setupjava9openjdk.sh to just setupjdk9.sh; helper to setupjdk-helper

---
+++ installers/setupjdk-helper.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Java JDK setup helper. This will also setup the alternatives system.
+
+# Remark: OpenJDK packages are at http://jdk.java.net, older version packages at https://jdk.java.net/archive/
+
+PROGNAME="setupjdk-helper.sh"
+
+echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
+
+# #############################################################################
+# Checks
+
+# if [ "$UID" != 0 ] ; then
+#   echo "FATAL: Must run this as root" 1>&2
+#   exit 1
+# fi
+
+if [ -z "${JDK_VER_MAJOR}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'JDK_VER_MAJOR' must be exported in caller." 1>&2 ; exit 1 ; fi
+if [ -z "${ALTERNATIVES_INDEX}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'ALTERNATIVES_INDEX' must be exported in caller." 1>&2 ; exit 1 ; fi
+if [ -z "${JDK_INSTALL_PATH}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'JDK_INSTALL_PATH' must be exported in caller." 1>&2 ; exit 1 ; fi
+if [ -z "${JDK_EXTRACTED_PATH}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'JDK_EXTRACTED_PATH' must be exported in caller." 1>&2 ; exit 1 ; fi
+if [ -z "${JDK_SHELL_PROFILE_PREFIX}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'JDK_SHELL_PROFILE_PREFIX' must be exported in caller." 1>&2 ; exit 1 ; fi
+if [ -z "${JDK_SHELL_PROFILE}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'JDK_SHELL_PROFILE' must be exported in caller." 1>&2 ; exit 1 ; fi
+if [ -z "${JDK_PACKAGE_URL}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'JDK_PACKAGE_URL' must be exported in caller." 1>&2 ; exit 1 ; fi
+if [ -z "${JDK_PATH}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'JDK_PATH' must be exported in caller." 1>&2 ; exit 1 ; fi
+if [ -z "${REQ_HEADER}" ] ; then echo "${PROGNAME:+$PROGNAME: }FATAL: Global 'REQ_HEADER' must be exported in caller." 1>&2 ; exit 1 ; fi
+
+# #############################################################################
+# Prep
+
+cd "${JDK_INSTALL_PATH}"
+
+sudo wget \
+  --no-cookies \
+  --no-check-certificate \
+  --header "${REQ_HEADER}" \
+  -O "${PACKAGE_URL##*/}" \
+  "${PACKAGE_URL}"
+
+# #############################################################################
+# Install
+
+sudo tar xzf "${PACKAGE_URL##*/}"
+
+# #############################################################################
+# Post installation configuration
+
+sudo ln -f -s "${JDK_EXTRACTED_PATH}" "${JDK_PATH}"
+cd "${JDK_PATH}"/
+
+if sudo which alternatives; then
+  # Highest update-alternatives priority for a newer JDK version being installed:
+  export ALTERNATIVES_INDEX=$((ALTERNATIVES_INDEX+JDK_VER_MAJOR))
+  # Switch Java binaries defaults:
+  sudo alternatives --install /usr/bin/java java "${JDK_PATH}"/bin/java "${ALTERNATIVES_INDEX}"
+  sudo alternatives --config java
+  sudo alternatives --install /usr/bin/jar jar "${JDK_PATH}"/bin/jar "${ALTERNATIVES_INDEX}"
+  sudo alternatives --install /usr/bin/javac javac "${JDK_PATH}"/bin/javac "${ALTERNATIVES_INDEX}"
+  sudo alternatives --set jar "${JDK_PATH}"/bin/jar
+  sudo alternatives --set javac "${JDK_PATH}"/bin/javac
+fi
+
+# TODO update-alternatives add manpages slaves
+
+# #############################################################################
+echo "Updating profile '${JDK_SHELL_PROFILE}'..." 1>&2
+
+for jdkprofile in $(ls "${JDK_SHELL_PROFILE##*/}/${JDK_SHELL_PROFILE_PREFIX}"*.sh 2>/dev/null) ; do
+  sudo mv -v "${jdkprofile}" "${jdkprofile%.sh}.disabled"
+done
+
+  cat >> "${JDK_SHELL_PROFILE}" <<EOF
+export JAVA_HOME=${JDK_PATH}
+export JRE_HOME=${JDK_PATH}/jre
+export PATH=\$PATH:\$JAVA_HOME/bin:\$JRE_HOME/bin
+EOF
+
+# #############################################################################
+# Verification
+
+. "${JDK_SHELL_PROFILE}"
+which java
+java -version || exit "$?"
+
+# #############################################################################
---
+++ installers/setupjdk9.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Remark: OpenJDK packages are at http://jdk.java.net, older version packages at https://jdk.java.net/archive/
+
+PROGNAME="setupjdk9.sh"
+
+export JDK_VER_MAJOR=9
+export JDK_VER_FULL=9.0.4
+
+export ALTERNATIVES_INDEX=9000
+export JDK_INSTALL_PATH=/usr/local
+export JDK_EXTRACTED_PATH="${JDK_INSTALL_PATH}/jdk-${JDK_VER_FULL}"
+export JDK_SHELL_PROFILE_PREFIX="jdk-"
+export JDK_SHELL_PROFILE="/etc/profile.d/${JDK_SHELL_PROFILE_PREFIX}${JDK_VER_MAJOR}.sh"
+export JDK_PACKAGE_URL="https://download.java.net/java/GA/jdk${JDK_VER_MAJOR}/${JDK_VER_FULL}/binaries/openjdk-${JDK_VER_FULL}_linux-x64_bin.tar.gz"
+export JDK_PATH="${JDK_INSTALL_PATH}/jdk-${JDK_VER_MAJOR}"
+export REQ_HEADER="Cookie: oraclelicense=accept-securebackup-cookie"
+
+# #############################################################################
+# Main
+
+./setupjdk-helper.sh
+echo "$PROGNAME: COMPLETE: Java OpenJDK setup"
+exit
