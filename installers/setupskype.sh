37d3b548ffcb7ec154148e76299608a8d03e8b49
Cristian Stroparo <stroparo@outlook.com>
2020-10-22 14:48:37 -0300

    Fix setupskype

--- installers/setupskype.sh
+++ installers/setupskype.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 PROGNAME="setupskype.sh"
+DEBFILE=/tmp/skypeforlinux-64.deb
+DEBURL_MAIN='https://go.skype.com/skypeforlinux-64.deb'
+DEBURL_ALTN='https://repo.skype.com/latest/skypeforlinux-64.deb'
 
 if ! (uname | grep -i -q linux) ; then echo "$PROGNAME: SKIP: Linux supported only" ; exit ; fi
+
+if type skype >/dev/null 2>&1 ; then
+  echo "${PROGNAME:+$PROGNAME: }SKIP: Skype already installed." 1>&2
+  exit
+fi
 
 echo "$PROGNAME: INFO: Skype setup started"
 echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
@@ -24,7 +32,10 @@ if egrep -i -q -r '(centos|fedora|oracle|red *hat).* 7' /etc/*release ; then
 
 elif egrep -i -q -r 'debian|ubuntu' /etc/*release ; then
 
-  sudo dpkg -i <(curl -LSfs https://go.skype.com/skypeforlinux-64.deb)
+  wget -O "${DEBFILE}" "${DEBURL_MAIN}" \
+    || wget -O "${DEBFILE}" "${DEBURL_ALTN}"
+
+  sudo dpkg -i "${DEBFILE}" && rm -f "${DEBFILE}"
 
 else
   echo "${PROGNAME:+$PROGNAME: }SKIP: OS not supported" 1>&2
