484082446a875a79e4ff61ac20ba0e4cb541221f
Cristian Stroparo <stroparo@outlook.com>
2020-12-11 22:35:50 -0300

    Update setupexa now suffices 'cargo install exa'

--- installers/setupexa.sh
+++ installers/setupexa.sh
@@ -8,48 +8,21 @@ if type exa >/dev/null 2>&1 ; then echo "${PROGNAME:+$PROGNAME: }SKIP: exa alrea
 echo "$PROGNAME: INFO: Setup exa setup started"
 echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 
-# #############################################################################
-# Functions
 
-_install_exa_deb () {
+_setup_exa () {
   [ ! -d ~/bin ] && ! mkdir ~/bin && exit 1
 
   # Dependency - Rust language platform:
   [ -d ~/.cargo/bin ] || (curl "https://sh.rustup.rs" -sSf | sh)
   export PATH="$HOME/.cargo/bin:$PATH"
 
-  # Dependency - APT packages:
-  sudo apt update || exit 1
-  sudo apt install -y libgit2-dev
-  sudo apt install -y libhttp-parser-dev || exit $?
-  which cmake >/dev/null 2>&1 || sudo apt install -y cmake || exit $?
-  which git >/dev/null 2>&1 || sudo apt install -y git || exit $?
-
-  # Compile:
-  git clone --depth 1 "https://github.com/ogham/exa.git" /tmp/exa \
-    && (cd /tmp/exa && make install)
-
   # Install:
-  if [ -f /tmp/exa/target/release/exa ] ; then
-    sudo cp /tmp/exa/target/release/exa /usr/local/bin/exa \
-      && ls -l /usr/local/bin/exa \
-      && rm -rf /tmp/exa
-  fi
+  cargo install exa
 }
 
-# #############################################################################
-# Main
 
-if egrep -i -q -r 'debian|ubuntu' /etc/*release ; then
-  _install_exa_deb "$@"
-  exit $?
-elif egrep -i -q -r 'centos|fedora|oracle|red *hat' /etc/*release ; then
-  echo "${PROGNAME:+$PROGNAME: }SKIP: _install_exa_rpm routine still to be implemented" 1>&2
-  exit
-else
-  echo "${PROGNAME:+$PROGNAME: }SKIP: this OS distribution is not handled" 1>&2
-  exit
-fi
+_setup_exa "$@"
+result=$?
 
 echo "${PROGNAME:+$PROGNAME: }COMPLETE: exa setup complete"
-exit
+exit $result
