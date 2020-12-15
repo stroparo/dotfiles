31af2ebb2c81164355d3a2e73164a5ce7675834c
Cristian Stroparo <stroparo@outlook.com>
2020-12-15 01:01:02 -0300

    Incorporate old apps-el recipe into the new cross-platform apps-cli

--- recipes/apps-cli.sh
+++ recipes/apps-cli.sh
@@ -1,21 +1,39 @@
 #!/bin/sh
 
-PROGNAME="apps-debian.sh"
+PROGNAME="apps-cli.sh"
 export APTPROG=apt-get
-export INSTPROG=apt-get
+export RPMPROG=yum; which dnf >/dev/null 2>&1 && export RPMPROG=dnf
+export RPMGROUP="yum groupinstall"; which dnf >/dev/null 2>&1 && export RPMGROUP="dnf group install"
+export PKG_LIST_FILE_EL="${RUNR_DIR}/assets/pkgs-el.lst"
+export PKG_LIST_FILE_EL_EPEL="${RUNR_DIR}/assets/pkgs-el-epel.lst"
 export PKG_LIST_FILE_UBUNTU="${RUNR_DIR}/assets/pkgs-ubuntu.lst"
+
+export INSTPROG="${APTPROG}"
 
 
 if grep -i -q -r 'debian' /etc/*release ; then
   isdebian=true
   export PKG_LIST_FILE="${PKG_LIST_FILE_UBUNTU}"
+
 elif grep -i -q -r 'ubuntu' /etc/*release ; then
   isubuntu=true
   export PKG_LIST_FILE="${PKG_LIST_FILE_UBUNTU}"
+
+elif egrep -i -q -r 'rhel|enterprise|centos|fedora|oracle linux' /etc/*release ; then
+  isel=true
+  export INSTPROG="$RPMPROG"
+  export PKG_LIST_FILE="${PKG_LIST_FILE_EL}"
+
 else
   echo "${PROGNAME:+$PROGNAME: }SKIP: OS not supported." 1>&2
   exit
 fi
+
+
+_is_el () { egrep -i -q -r '(cent *os|oracle|red *hat)' /etc/*release ; }
+_is_el6 () { egrep -i -q -r '(cent *os|oracle|red *hat).* 6' /etc/*release ; }
+_is_el7 () { egrep -i -q -r '(cent *os|oracle|red *hat).* 7' /etc/*release ; }
+_is_fedora () { egrep -i -q -r 'fedora' /etc/*release ; }
 
 
 _assemble_valid_pkg_list () {
@@ -29,6 +47,33 @@ _assemble_valid_pkg_list () {
 }
 
 
+_prep_el () {
+  # Package indexing setup
+  sudo yum makecache fast
+
+  if ! _is_fedora ; then
+    echo "PROGNAME: INFO: EPEL - Extra Packages for Enterprise Linux..." # https://fedoraproject.org/wiki/EPEL
+    if _is_el7 ; then
+      _install_packages https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    else
+      _install_packages epel-release.noarch
+    fi
+  fi
+
+  sudo $RPMGROUP -q -y --enablerepo=epel 'Development Tools'
+}
+
+
+_install_epel_packages () {
+  typeset timestamp="$(date '+%Y%m%d-%OH%OM%OS')"
+
+  echo "$PROGNAME: INFO: Installing EPEL packages..."
+  if ! (sudo "${INSTPROG}" install -y --enablerepo=epel "$@" 2>&1 | tee "/tmp/pkg-install-epel-${timestamp}.log") ; then
+    echo "${PROGNAME:+$PROGNAME: }WARN: There was an error installing packages - see '/tmp/pkg-install-epel-${timestamp}.log'." 1>&2
+  fi
+}
+
+
 _install_packages () {
   typeset timestamp="$(date '+%Y%m%d-%OH%OM%OS')"
 
@@ -36,7 +81,18 @@ _install_packages () {
   echo
   echo "$PROGNAME: INFO: Installing packages..."
   if ! (sudo "${INSTPROG}" install -y $(_assemble_valid_pkg_list "${PKG_LIST_FILE}") 2>&1 | tee "/tmp/pkg-install-${timestamp}.log") ; then
-    echo "$PROGNAME: WARN: There was an error installing package '${package}' - see '/tmp/pkg-install-${timestamp}.log'." 1>&2
+    echo "$PROGNAME: WARN: There was an error installing packages - see '/tmp/pkg-install-${timestamp}.log'." 1>&2
+  fi
+}
+
+
+_cleanup () {
+  if ${isdebian:-false} || ${isubuntu:-false} ; then
+    echo
+    echo
+    echo "$PROGNAME: INFO: APT repository clean up (autoremove & clean)..."
+    sudo $APTPROG autoremove -y
+    sudo $APTPROG clean -y
   fi
 }
 
@@ -49,17 +105,13 @@ if ${isdebian:-false} || ${isubuntu:-false} ; then
   if ! (sudo $APTPROG update | tee '/tmp/apt-update-err.log') ; then
     echo "$PROGNAME: WARN: There was some failure during APT index update - see '/tmp/apt-update-err.log'." 1>&2
   fi
+elif ${isel} ; then
+  _prep_el
+  _install_epel_packages $(_assemble_valid_pkg_list "${PKG_LIST_FILE_EL_EPEL}")
 fi
 
 _install_packages
-
-if ${isdebian:-false} || ${isubuntu:-false} ; then
-  echo
-  echo
-  echo "$PROGNAME: INFO: APT repository clean up (autoremove & clean)..."
-  sudo $APTPROG autoremove -y
-  sudo $APTPROG clean -y
-fi
+_cleanup
 
 echo
 echo
