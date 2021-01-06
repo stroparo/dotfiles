ae6e1f8c9db4a220261d456213c95161e10dc3c0
Cristian Stroparo <stroparo@outlook.com>
2021-01-06 00:30:54 -0300

    Improve setupdocker-compose python/pyenv handling, and default alternate version to 1.27.4

--- installers/setupdocker-compose.sh
+++ installers/setupdocker-compose.sh
@@ -10,22 +10,26 @@ echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 # #############################################################################
 # Globals
 
+DC_VERSION='1.27.4'
+DC_ALTN_URL="https://github.com/docker/compose/releases/download/${DC_VERSION}/docker-compose-`uname -s`-`uname -m`"
+
 COMPLETION_FILE="${HOME}/.zsh/completion/_docker-compose"
-COMPLETION_URL="https://raw.githubusercontent.com/docker/compose/1.18.0/contrib/completion/zsh/_docker-compose"
+
+SYSTEM_PYTHON="$(ls -1 /bin/python3 2>/dev/null || ls -1 /usr/bin/python3 2>/dev/null || echo python3)"
 
 # #############################################################################
 # Install
 
-if type docker-compose >/dev/null 2>&1 ; then
-  echo "${PROGNAME:+$PROGNAME: }SKIP: Already installed." 1>&2
-  exit
-elif which pip >/dev/null 2>&1 ; then
-  pip install --user docker-compose
+if which pip >/dev/null 2>&1 ; then
+  if type pyenv >/dev/null 2>&1 ; then
+    pyenv local system
+  fi
+  ${SYSTEM_PYTHON:-python3} -m pip install --user --upgrade docker-compose
 fi
 
 # Download docker-compose as a last resort if all else fails:
 if ! type docker-compose >/dev/null 2>&1 ; then
-  sudo curl ${DLOPTEXTRA} -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo curl ${DLOPTEXTRA} -L "${DC_ALTN_URL}" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
@@ -33,13 +37,18 @@ fi
 # Verification
 
 echo
-if ! docker-compose --version ; then
+
+# Update DC_VERSION in case the pip installation succeeded:
+DC_VERSION="$(docker-compose --version | egrep -o 'version [0-9]+[.][0-9]+[.][0-9]+' | sed -e 's/version //')"
+if [ $? -ne 0 ] ; then
   echo "${PROGNAME:+$PROGNAME: }FATAL: docker-compose is not available." 1>&2
   exit 1
 fi
 
 # #############################################################################
 # Install completions
+
+COMPLETION_URL="https://raw.githubusercontent.com/docker/compose/${DC_VERSION}/contrib/completion/zsh/_docker-compose"
 
 if which zsh >/dev/null 2>&1 ; then
 
