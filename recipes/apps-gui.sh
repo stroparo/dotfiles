f35a1b104a0640ed6092021a8024bde0a582c229
Cristian Stroparo <stroparo@outlook.com>
2021-06-06 20:54:27 -0300

    Format

--- recipes/apps-gui.sh
+++ recipes/apps-gui.sh
@@ -14,37 +14,33 @@ source "${RUNR_DIR:-.}"/helpers/dsenforce.sh
 
 cd "${RUNR_DIR:-$PWD}"
 
-
+# #############################################################################
 if _is_debian_family ; then
   export PKG_LIST_FILE="${PKG_LIST_FILE_UBUNTU}"
 
   if ! (sudo "$APTPROG" update | tee '/tmp/apt-update-err.log') ; then
     echo "$PROGNAME: WARN: There was some failure during APT index update - see '/tmp/apt-update-err.log'." 1>&2
   fi
-
+# #############################################################################
 elif _is_el_family ; then
   export PKG_LIST_FILE="${PKG_LIST_FILE_EL}"
 
   installrepoepel
   sudo $RPMGROUP -q -y --enablerepo=epel 'Development Tools'
   # installpkgsepel $(validpkgs "${PKG_LIST_FILE_EL_EPEL}")
-
+# #############################################################################
 else
   echo "${PROGNAME:+$PROGNAME: }SKIP: OS not supported." 1>&2
   exit
 fi
-
+# #############################################################################
 
 installpkgs $(validpkgs "${PKG_LIST_FILE}")
-
-
 bash "${RUNR_DIR:-$PWD}"/recipes/apps-ubuntu-ppa.sh
-
 
 if _is_debian_family ; then
   aptcleanup
 fi
-
 
 echo
 echo
