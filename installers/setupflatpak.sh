30845090b63fdfd50d8d4ea8e3fc8b2267bc4986
Cristian Stroparo <stroparo@outlook.com>
2021-10-11 20:48:42 -0300

    Update setupflatpak add post-installation check

--- installers/setupflatpak.sh
+++ installers/setupflatpak.sh
@@ -6,8 +6,11 @@ echo "$PROGNAME: INFO: setup started..."
 echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 
 if grep -q 'ubuntu' /etc/*release ; then
-  sudo apt-get install flatpak
-  flatpak remote-add --if-not-exists flathub 'https://flathub.org/repo/flathub.flatpakrepo'
+  sudo apt-get update \
+  && sudo apt-get install -y flatpak
+  if type flatpak >/dev/null 2>&1 ; then
+    flatpak remote-add --if-not-exists flathub 'https://flathub.org/repo/flathub.flatpakrepo'
+  fi
 else
   echo "${PROGNAME:+$PROGNAME: }SKIP: this OS is not handled by this script." 1>&2
   exit
