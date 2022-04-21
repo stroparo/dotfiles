30839e5b2d6bc51ffc41c59764a9885fecbdc6e0
Cristian Stroparo <stroparo@outlook.com>
2022-04-20 23:10:13 -0300

    Add Arch Linux to installers/setupvscode

--- installers/setupvscode.sh
+++ installers/setupvscode.sh
@@ -8,31 +8,21 @@ if ! (uname | grep -i -q linux) ; then echo "$PROGNAME: SKIP: Linux supported on
 echo "$PROGNAME: INFO: setup started..."
 echo "$PROGNAME: INFO: \$0='$0'; \$PWD='$PWD'"
 
-if which code >/dev/null 2>&1 ; then
-  echo "$PROGNAME: INFO: already installed, so trying to upgrade..."
+if egrep -i -q -r 'id[^=]*=arch' /etc/*release ; then
+  yay -Sy visual-studio-code-bin
 
-  # TODO handle distros
-
-  sudo apt-get update
-  sudo apt-get install -y code
-  exit
+elif egrep -i -q -r 'debian|ubuntu' /etc/*release ; then
+  echo "$PROGNAME: INFO: starting browser with the official download URL..."
+  if which firefox ; then
+    firefox "${VSCODEURL}" & disown
+  elif which google-chrome ; then
+    google-chrome "${VSCODEURL}" & disown
+  elif which brave-browser ; then
+    brave-browser "${VSCODEURL}" & disown
+  fi
+  read -p "Hit ENTER when ready to install (i.e. package has been downloaded to '${HOME}/Downloads/')..." dummy
+  sudo gdebi "$(ls -1 ~/Downloads/code*.deb | tail -n 1)"
 fi
-
-
-# TODO handle distros
-# Arch: yay -Sy visual-studio-code-bin
-
-echo "$PROGNAME: INFO: starting browser with the official download URL..."
-if which firefox ; then
-  firefox "${VSCODEURL}" & disown
-elif which google-chrome ; then
-  google-chrome "${VSCODEURL}" & disown
-elif which brave-browser ; then
-  brave-browser "${VSCODEURL}" & disown
-fi
-read -p "Hit ENTER when ready to install (i.e. package has been downloaded to '${HOME}/Downloads/')..." dummy
-sudo gdebi "$(ls -1 ~/Downloads/code*.deb | tail -n 1)"
-
 
 echo "${PROGNAME:+$PROGNAME: }COMPLETE"
 echo
